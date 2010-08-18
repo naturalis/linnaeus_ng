0b131105bffa86457b736b22b17ba67b3d9b9018
Maarten Schermer <maarten.schermer@naturalis.nl>
2010-08-18 16:03:41 +0000

    basic upload

---
+++ www/admin/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Linnaeus NG</title>
+<script src="javascript/loadshared.js" type="text/javascript"></script>
+</head>
+
+<body>
+admin-root<br /><br />
+
+<a href="../">www-root</a><br/>
+<a href="../app/">user interface</a><br/>
+
+</body>
+</html>
