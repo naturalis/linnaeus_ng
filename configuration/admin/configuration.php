2384472679a8708ece2f609e237468e7144d989b
Maarten Schermer <maarten.schermer@naturalis.nl>
2010-08-19 13:58:21 +0000

    basic upload

---
+++ configuration/admin/configuration.php
@@ -0,0 +1,35 @@
+<?php
+
+	class configuration {
+
+		const smartyRoot = 'C:/Users/maarten/htdocs/linnaeus ng/linnaeus_ng/www/admin/templates/';
+
+		public function getDatabaseSettings() {
+
+			return array(
+				'host' => '127.0.0.1',
+				'user' => 'linnaeus_user',
+				'password' => 'car0lu5',
+				'database' => 'linnaeus_ng',
+				'tablePrefix' => 'dev_',
+				'characterSet' => 'utf8'
+			);
+
+		}
+
+		public function getSmartySettings() {
+
+			return array(
+				'dir_template' => self::smartyRoot.'templates',
+				'dir_compile' => self::smartyRoot.'templates_c',
+				'dir_cache' => self::smartyRoot.'cache',
+				'dir_config' => self::smartyRoot.'configs',
+				'caching' => 1, // 1,
+				'compile_check' => true
+			);
+
+		}
+
+	}
+
+?>
