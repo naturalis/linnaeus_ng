40a185bb9461a9ca82b66718c2ef4d1132597bcb
Maarten Schermer <maarten.schermer@naturalis.nl>
2010-08-26 10:07:46 +0000

    LINNG-28

--- www/index.php
+++ www/index.php
@@ -2,14 +2,31 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Linnaeus NG</title>
+<title>Login</title>
+<style type="text/css" media="all">
+  @import url("/admin/style/main.css");
+  @import url("/admin/style/admin.css");
+</style>
+<script type="text/javascript" src="/admin/javascript/main.js"></script>
 </head>
+<body><div id="admin-body-container">
+<div id="admin-header-container">
+	<img src="/admin/images/system/eti-logo.png" id="admin-page-eti-logo" />
+</div>
+<div id="admin-page-container">
 
-<body>
-www-root<br /><br />
+<div id="admin-titles">
+	<span id="admin-title">Linnaeus NG</span><br />
+	<span id="admin-subtitle">webroot</span><br />
+</div>
 
-<a href="admin/">admin interface</a><br/>
-<a href="app/">user interface</a><br/>
+<div id="admin-main">
+	<a href="/admin/">Administration</a><br />
+	<a href="/app/">Application</a>
+</div>
 
-</body>
+</div ends="admin-page-container">
+<div id="admin-footer-container">
+</div ends="admin-footer-container">
+</div ends="admin-body-container"></body>
 </html>
