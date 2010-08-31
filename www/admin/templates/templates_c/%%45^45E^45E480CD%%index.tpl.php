505abda804246e5fe0bd35a7c2bf7d9787e7cbde
Maarten Schermer <maarten.schermer@naturalis.nl>
2010-08-31 17:40:07 +0000

    LINNG-33 / LINNG-34 / LINNG-36

--- www/admin/templates/templates_c/%%45^45E^45E480CD%%index.tpl.php
+++ www/admin/templates/templates_c/%%45^45E^45E480CD%%index.tpl.php
@@ -1,4 +1,4 @@
-<?php /* Smarty version 2.6.26, created on 2010-08-31 18:29:11
+<?php /* Smarty version 2.6.26, created on 2010-08-31 19:38:41
          compiled from index.tpl */ ?>
 <?php $_smarty_tpl_vars = $this->_tpl_vars;
 $this->_smarty_include(array('smarty_include_tpl_file' => "../shared/admin-header.tpl", 'smarty_include_vars' => array()));
