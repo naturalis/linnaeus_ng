dcf06c6f36b415993f210c623029c3184264421d
Maarten Schermer <maarten.schermer@naturalis.nl>
2010-09-21 16:13:24 +0000

    LINNG-69

--- www/index.php
+++ www/index.php
@@ -1,25 +1,34 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title></title>
-<style type="text/css" media="all">
-@import url("admin/style/main.css");
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-@import url("admin/style/admin-inputs.css");
+	<title>Login</title>
 
-@import url("admin/style/admin-help.css");
+	<link href="/admin/images/system/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+	<link href="/admin/images/system/favicon.ico" rel="icon" type="image/x-icon" />
 
-@import url("admin/style/admin.css");
-</style>
-<script type="text/javascript"
-	src="admin/javascript/jquery-1.4.2.min.js"></script>
-<script type="text/javascript" src="admin/javascript/main.js"></script>
+	<style type="text/css" media="all">
+		@import url("/admin/style/main.css");
+		@import url("/admin/style/admin-inputs.css");
+		@import url("/admin/style/admin-help.css");
+		@import url("/admin/style/admin.css");
+	</style>
+
+	<script type="text/javascript" src="/admin/javascript/jquery-1.4.2.min.js"></script>
+	<script type="text/javascript" src="/admin/javascript/main.js"></script>
+
+
 </head>
-<body>
-<div id="body-container">
-<div id="header-container"><img
-	src="admin/images/system/linnaeus_logo.png" id="eti-logo" /></div>
+
+<body><div id="body-container">
+<div id="header-container">
+	<a href="/admin/admin-index.php"><img src="/admin/images/system/linnaeus_logo.png" id="lng-logo" />
+	<img src="/admin/images/system/eti_logo.png" id="eti-logo" /></a>
+
+
+</div>
+
 <div id="page-container">
 
 <div id="page-header-titles"><span id="page-header-title">Linnaeus NG</span><br />
@@ -27,7 +36,7 @@
 
 <div id="page-main">Welcome to Linnaeus NG.<br />
 <br />
-To use the administration, follow <a href="admin/">this link</a>.<br />
+To use the administration, follow <a href="/admin/views/users/login.php">this link</a>.<br />
 To use the application, follow <a href="app/">this link</a>.<br />
 <br />
 </div>
