943af36e8af61f529c95cda0c968f704946fd79d
Maarten Schermer <maarten.schermer@naturalis.nl>
2011-01-05 11:04:23 +0000

    LINNG-159

---
+++ configuration/admin/controllers/HighertaxaController.php
@@ -0,0 +1,55 @@
+<?php
+
+include_once ('Controller.php');
+
+class HighertaxaController extends Controller
+{
+
+    public $controllerPublicName = 'Higher taxa';
+
+
+    /**
+     * Constructor, calls parent's constructor
+     *
+     * @access     public
+     */
+    public function __construct ()
+    {
+
+        parent::__construct();
+
+    }
+
+    /**
+     * Destroys
+     *
+     * @access     public
+     */
+    public function __destruct ()
+    {
+        
+        parent::__destruct();
+    
+    }
+
+
+    /**
+     * Index of the species module
+     *
+     * @access    public
+     */
+    public function indexAction ()
+    {
+
+        $this->checkAuthorisation();
+
+		$_SESSION['system']['highertaxa'] = true;
+        
+        $this->setPageName(_('Higher taxa overview'));
+
+        $this->printPage();
+  
+    }
+
+}
+
