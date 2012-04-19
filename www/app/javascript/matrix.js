f67c99309d8fd4a25a560d6e473376912d234689
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2012-04-19 10:58:28 +0000

    matrix.js

--- www/app/javascript/matrix.js
+++ www/app/javascript/matrix.js
@@ -1,25 +1,23 @@
 var imagePath;
 
 function getData(action,id,postFunction) {
-//commentaar
-//prompt('hoer','ajax_interface.php?action='+action+'&id='+id+'&inc_unknowns='+$('#unknowns-y').attr('checked')+);
 
-        allAjaxHandle = $.ajax({
-                url : 'ajax_interface.php',
-                type: 'POST',
-                data : ({
-                        'action' : action ,
-                        'id' : id ,
-                        'inc_unknowns' : $('#unknowns-y').attr('checked') ? 1 : 0 ,
-                        'time' : getTimestamp()
-                }),
-                success : function (data) {
-                        //alert(data);
-                        obj = $.parseJSON(data);
-                        eval(postFunction+'(obj,id)');
-                }
-        })
-       
+	allAjaxHandle = $.ajax({
+		url : 'ajax_interface.php',
+		type: 'POST',
+		data : ({
+			'action' : action ,
+			'id' : id , 
+			'inc_unknowns' : $('#unknowns-y').attr('checked') ? 1 : 0 , 
+			'time' : getTimestamp()
+		}),
+		success : function (data) {
+			//alert(data);
+			obj = $.parseJSON(data);
+			eval(postFunction+'(obj,id)');
+		}
+	})
+	
 }
 
 var characteristics = Array();
@@ -28,342 +26,341 @@ var selected = Array();
 
 function storeCharacteristic(id,label,char) {
 
-        characteristics[id] = [label,char];
+	characteristics[id] = [label,char];
 
 }
 
 function fillStates(obj,char) {
 
-        $('#states').empty();
+	$('#states').empty();
 
-        setInfo(' ');
+	setInfo(' ');
 
-        if (!obj) return;
+	if (!obj) return;
 
-        for(var i=0;i<obj.length;i++) {
+	for(var i=0;i<obj.length;i++) {
 
-                if (obj[0].type.name != 'range' && obj[0].type.name != 'distribution') {
-       
-                        $('#states').append('<option value="'+obj[i].id+'">'+obj[i].label+'</option>').val(obj[i].id);
-       
-                }
-               
-                states[obj[i].id] = obj[i];
+		if (obj[0].type.name != 'range' && obj[0].type.name != 'distribution') {
+	
+			$('#states').append('<option value="'+obj[i].id+'">'+obj[i].label+'</option>').val(obj[i].id);
+	
+		}
+		
+		states[obj[i].id] = obj[i];
 
-        }
+	}
 
-        if (obj[0].type.name != 'range' && obj[0].type.name != 'distribution') {
+	if (obj[0].type.name != 'range' && obj[0].type.name != 'distribution') {
 
-                $("#states :first").attr('selected','selected');
+		$("#states :first").attr('selected','selected');
 
-                goState();
+		goState();
 
-        } else {
+	} else {
 
-                setInfo(
-                        '<b>'+$('#characteristics :selected').text()+'</b><br />'+
-                        sprintf(
-                                _('Click %shere%s to specify a value; you can also double-click "%s" to do so.'),
-                                '<span class="a" onclick="addSelected($(\'#characteristics\'))">',
-                                '</span>',
-                                $('#characteristics :selected').text()
-                        )
-                );
+		setInfo(
+			'<b>'+$('#characteristics :selected').text()+'</b><br />'+
+			sprintf(
+				_('Click %shere%s to specify a value; you can also double-click "%s" to do so.'),
+				'<span class="a" onclick="addSelected($(\'#characteristics\'))">',
+				'</span>',
+				$('#characteristics :selected').text()
+			)
+		);
 
-        }
+	}
 
 }
 
 function setInfo(val) {
 
-        $('#info').html(val);
+	$('#info').html(val);
 
 }
 
 function goState() {
 
-        var state = states[$('#states').val()];
+	var state = states[$('#states').val()];
 
-//      alert(dumpObj(state));
-       
-        switch (state.type.name) {
-                case 'text':
-                        var val = state.text;
-                        break;
-                case 'media':
-                        var file = encodeURIComponent(state.file_name);
-                        var val = '<img alt="'+file+'" onclick="showMedia(\''+imagePath+file+'\',\''+file+'\');" src="'+imagePath+state.file_name+'" class="info-image" /><br />'+_('(click image to enlarge)');
-                        break;
-                case 'range':
-                        var val =      
-                                _('lower: ')+state.lower+'<br />'+
-                                _('upper: ')+state.upper+'<br />';
-                        break;
-                case 'distribution':
-                        var val =      
-                                _('mean: ')+state.mean+'<br />'+
-                                _('sd: ')+state.sd+'<br />';
-                break;
-        }
+//	alert(dumpObj(state));
+	
+	switch (state.type.name) {
+		case 'text':
+			var val = state.text;
+			break;
+		case 'media':
+			var file = encodeURIComponent(state.file_name);
+			var val = '<img alt="'+file+'" onclick="showMedia(\''+imagePath+file+'\',\''+file+'\');" src="'+imagePath+state.file_name+'" class="info-image" /><br />'+_('(click image to enlarge)');
+			break;
+		case 'range':
+			var val = 	
+				_('lower: ')+state.lower+'<br />'+
+				_('upper: ')+state.upper+'<br />';
+			break;
+		case 'distribution':
+			var val = 	
+				_('mean: ')+state.mean+'<br />'+
+				_('sd: ')+state.sd+'<br />';
+		break;
+	}
 
-        setInfo(val ? '<span class="info-header">'+characteristics[state.characteristic_id][0]+': '+state.label+'</span><br/>'+val : ' ');
+	setInfo(val ? '<span class="info-header">'+characteristics[state.characteristic_id][0]+': '+state.label+'</span><br/>'+val : ' ');
 
 }
 
 function goCharacteristic() {
 
-        getData('get_states',$('#characteristics').val(),'fillStates');
+	getData('get_states',$('#characteristics').val(),'fillStates');
 
 }
 
-var strOpen =
-        '<table style="font-size:11px;">'+
-                '<tr style="height:30px;vertical-align:top">'+
-                        '<td colspan=2>%s</td>'+
-                '</tr>'+
-                '<tr>'+
-                        '<td>'+_('Value:')+'</td>'+
-                        '<td><input type=text id=dialogValue style="font-size:12px;width:35px;text-align:right" /></td>'+
-                '</tr>';
+var strOpen = 
+	'<table style="font-size:11px;">'+
+		'<tr style="height:30px;vertical-align:top">'+
+			'<td colspan=2>%s</td>'+
+		'</tr>'+
+		'<tr>'+
+			'<td>'+_('Value:')+'</td>'+
+			'<td><input type=text id=dialogValue style="font-size:12px;width:35px;text-align:right" /></td>'+
+		'</tr>';
 
-var strClose =
-        '<tr style="height:50px;vertical-align:bottom">'+
-                '<td colspan=2>'+
-                        '<input type=button value="'+_('ok')+'" onclick="doDialog();" >'+
-                        '<input type=button value="'+_('cancel')+'" onclick="$(\'#dialog-close\').click();">'+
-                '</td>'+
-        '</tr>'+
+var strClose = 
+	'<tr style="height:50px;vertical-align:bottom">'+
+		'<td colspan=2>'+
+			'<input type=button value="'+_('ok')+'" onclick="doDialog();" >'+
+			'<input type=button value="'+_('cancel')+'" onclick="$(\'#dialog-close\').click();">'+
+		'</td>'+
+	'</tr>'+
 '</table>'+
 '<script>'+
-'       $("#dialogValue").keypress(function(e) {'+
-'               if(e.keyCode == 13) {'+
-'                       doDialog();'+
-'               }'+
-'       });'+
+'	$("#dialogValue").keypress(function(e) {'+
+'		if(e.keyCode == 13) {'+
+'			doDialog();'+
+'		}'+
+'	});'+
 '</script>';
-               
-var strDistro =
-        strOpen +
-        '<tr>'+
-        '<td style="padding-right:10px">'+_('Number of allowed standard deviations:')+'</td>'+
-                '<td><select id=dialogSD style="font-size:11px;"><option selected>1</option><option>2</option><option>3</option></select></td>'+
-        '</tr>'+
-        strClose;
+		
+var strDistro = 
+	strOpen +
+	'<tr>'+
+	'<td style="padding-right:10px">'+_('Number of allowed standard deviations:')+'</td>'+
+		'<td><select id=dialogSD style="font-size:11px;"><option selected>1</option><option>2</option><option>3</option></select></td>'+
+	'</tr>'+
+	strClose;
 
-var strRange =
-        strOpen +
-        strClose;
+var strRange = 
+	strOpen +
+	strClose;
 
 
 function doDialog() {
-       
-        var v = $('#dialogValue').val();
-       
-        if (v.length==0) {
-               
-                alert(_('Please enter a value'));
-                $('#dialogValue').focus();
-                return;
-               
-        }
-        if (isNaN(parseInt(v))) {
-               
-                alert(_('Please enter a valid number'));
-                $('#dialogValue').val('');
-                $('#dialogValue').focus();
-                return;
-               
-        }
+	
+	var v = $('#dialogValue').val();
+	
+	if (v.length==0) {
+		
+		alert(_('Please enter a value'));
+		$('#dialogValue').focus();
+		return;
+		
+	}
+	if (isNaN(parseInt(v))) {
+		
+		alert(_('Please enter a valid number'));
+		$('#dialogValue').val('');
+		$('#dialogValue').focus();
+		return;
+		
+	}
 
-        setFreeValues([v,$('#dialogSD').val()]);
+	setFreeValues([v,$('#dialogSD').val()]);
 
-        $('#dialog-close').click();
+	$('#dialog-close').click();
 
 }
 
 function setFreeValues(vals) {
 
-        var c = characteristics[$('#characteristics').val()];
+	var c = characteristics[$('#characteristics').val()];
 
-        if (c[1]=='range')
-                $('#selected').append('<option id="f'+(Math.floor(Math.random()*11))+'" value="f:'+$('#characteristics').val()+':'+(vals[0])+'">'+c[0]+': '+vals[0]+'</option>');
-        else
-                $('#selected').append('<option id="f'+(Math.floor(Math.random()*11))+'" value="f:'+$('#characteristics').val()+':'+(vals[0])+':'+(vals[1])+'">'+c[0]+': '+_('mean')+' '+vals[0]+' &plusmn; '+vals[1]+' '+_('sd')+'</option>');
+	if (c[1]=='range')
+		$('#selected').append('<option id="f'+(Math.floor(Math.random()*11))+'" value="f:'+$('#characteristics').val()+':'+(vals[0])+'">'+c[0]+': '+vals[0]+'</option>');
+	else
+		$('#selected').append('<option id="f'+(Math.floor(Math.random()*11))+'" value="f:'+$('#characteristics').val()+':'+(vals[0])+':'+(vals[1])+'">'+c[0]+': '+_('mean')+' '+vals[0]+' &plusmn; '+vals[1]+' '+_('sd')+'</option>');
 
-        getScores();
+	getScores();
 
 }
 
 function addSelected(caller) {
 
-        var c = characteristics[$('#characteristics').val()];
+	var c = characteristics[$('#characteristics').val()];
 
-        if (caller.id=='characteristics' && (c[1]!='distribution' && c[1]!='range')) {
+	if (caller.id=='characteristics' && (c[1]!='distribution' && c[1]!='range')) {
 
-                return;
+		return;
 
-        }
+	}
 
-        if (c[1]=='distribution') {
+	if (c[1]=='distribution') {
 
-                showDialog(sprintf(strDistro,sprintf(_('Enter the required values for "%s":'),c[0])));
-                $('#dialogValue').focus();
+		showDialog(sprintf(strDistro,sprintf(_('Enter the required values for "%s":'),c[0])));
+		$('#dialogValue').focus();
 
-        } else
-        if (c[1]=='range') {
+	} else
+	if (c[1]=='range') {
 
-                showDialog(sprintf(strRange,sprintf(_('Enter the required value for "%s":'),c[0])));
-                $('#dialogValue').focus();
+		showDialog(sprintf(strRange,sprintf(_('Enter the required value for "%s":'),c[0])));
+		$('#dialogValue').focus();
 
-        } else {
-               
-                var s = states[$('#states').val()];
+	} else {
+		
+		var s = states[$('#states').val()];
 
-                if (s && (selected[s.id]==false || selected[s.id]==undefined)) {
-       
-                        $('#selected').
-                                append('<option id="s'+s.id+'" value="c:'+s.characteristic_id+':'+s.id+'">'+characteristics[s.characteristic_id][0]+': '+s.label+'</option>').
-                                val(s.id);
-                        selected[s.id] = true;
-       
-                }
-               
-                getScores();
+		if (s && (selected[s.id]==false || selected[s.id]==undefined)) {
+	
+			$('#selected').
+				append('<option id="s'+s.id+'" value="c:'+s.characteristic_id+':'+s.id+'">'+characteristics[s.characteristic_id][0]+': '+s.label+'</option>').
+				val(s.id);
+			selected[s.id] = true;
+	
+		}
+		
+		getScores();
 
-        }
+	}
 
 }
 
 function deleteSelected() {
 
-        var id = $('#selected').val();
+	var id = $('#selected').val();
 
-        if (id.substr(0,1)!='f') {
+	if (id.substr(0,1)!='f') {
 
-                selected[id] = false;
+		selected[id] = false;
 
-        }
+	}
 
-        $('#selected').children(':selected').remove();
+	$('#selected').children(':selected').remove();
 
-        getScores($('#selected').children().length==0 ? 'clear' : null);
+	getScores($('#selected').children().length==0 ? 'clear' : null);
 
 }
 
 function clearSelected() {
 
-        $('#selected').empty();
+	$('#selected').empty();
 
-        selected = selected.splice(0,0);
+	selected = selected.splice(0,0);
 
-        getScores('clear');
+	getScores('clear');
 
 }
 
 function getScores(action) {
 
-        if (action!='clear') {
+	if (action!='clear') {
 
-                var opt = Array();
-       
-                $('#selected').children().each(function(){
-                        opt[opt.length] = ($(this).val());
-                });
+		var opt = Array();
+	
+		$('#selected').children().each(function(){
+			opt[opt.length] = ($(this).val());
+		});
 
-        } else {
+	} else {
 
-                opt = -1;
+		opt = -1;
 
+	}
 
-        }
-
-        getData('get_taxa',opt,'fillScores');
+	getData('get_taxa',opt,'fillScores');
 
 }
 
 function fillScores(obj,char) {
 
-        $('#scores').empty();
+	$('#scores').empty();
 
-        if (!obj) return;
+	if (!obj) return;
 
-        for(var i=0;i<obj.length;i++) {
+	for(var i=0;i<obj.length;i++) {
 
-                $('#scores').
-                        append('<option ondblclick="'+(obj[i].type=='matrix' ? 'goMatrix' : 'goTaxon')+'('+obj[i].id+');" value="'+obj[i].id+'">'+
-                                (obj[i].score!=undefined ? obj[i].score+': ' : '')+
-                                (obj[i].type=='matrix' ? _('Matrix: ')+obj[i].name : obj[i].taxon)+'</option>').
-                        val(obj[i].id);
+		$('#scores').
+			append('<option ondblclick="'+(obj[i].type=='matrix' ? 'goMatrix' : 'goTaxon')+'('+obj[i].id+');" value="'+obj[i].id+'">'+
+				(obj[i].score!=undefined ? obj[i].score+': ' : '')+
+				(obj[i].type=='matrix' ? _('Matrix: ')+obj[i].name : obj[i].taxon)+'</option>').
+			val(obj[i].id);
 
-        }
+	}
 
 }
 
 
 function fillTaxonStates(obj,char) {
 
-        $('#states tbody tr').remove();
+	$('#states tbody tr').remove();
 
-        if (!obj) return;
+	if (!obj) return;
 
-        for(var i in obj) {
-                $('#states').append('<tr class="highlight"><td>'+obj[i].type.name+'</td><td>'+obj[i].characteristic+'</td><td>'+obj[i].state.label+'</td></tr>');
+	for(var i in obj) {
+		$('#states').append('<tr class="highlight"><td>'+obj[i].type.name+'</td><td>'+obj[i].characteristic+'</td><td>'+obj[i].state.label+'</td></tr>');
 
-        }
+	}
 
 }
 
 function goExamine() {
 
-        getData('get_taxon_states',$('#taxon-list').val(),'fillTaxonStates');
+	getData('get_taxon_states',$('#taxon-list').val(),'fillTaxonStates');
 
 }
 
 function goCompare() {
 
-        var id1 = $('#taxon-list-1').val();
-        var id2 = $('#taxon-list-2').val();
+	var id1 = $('#taxon-list-1').val();
+	var id2 = $('#taxon-list-2').val();
 
-        if (id1=='' || id2=='') {
+	if (id1=='' || id2=='') {
 
-                alert(_('You must select two taxa.'));
-                return;
+		alert(_('You must select two taxa.'));
+		return;
 
-        } else
-        if (id1==id2) {
+	} else
+	if (id1==id2) {
 
-                alert(_('You cannot compare a taxon to itself.'));
-                return;
+		alert(_('You cannot compare a taxon to itself.'));
+		return;
 
-        }
-        getData('compare',[id1,id2],'fillTaxaStates');
+	}
+	getData('compare',[id1,id2],'fillTaxaStates');
 
 }
-       
+	
 function fillTaxaStates(obj) {
-                       
-        $('#count-both').html(obj.both);
-        $('#taxon-1').html(obj.taxon_1.taxon);
-        $('#count-1').html(obj.count_1);
-        $('#taxon-2').html(obj.taxon_2.taxon);
-        $('#count-2').html(obj.count_2);
-        $('#count-total').html(obj.total);
-        $('#count-neither').html(obj.neither);
-        $('#coefficient').html(obj.coefficients[0].value);
+			
+	$('#count-both').html(obj.both);
+	$('#taxon-1').html(obj.taxon_1.taxon);
+	$('#count-1').html(obj.count_1);
+	$('#taxon-2').html(obj.taxon_2.taxon);
+	$('#count-2').html(obj.count_2);
+	$('#count-total').html(obj.total);
+	$('#count-neither').html(obj.neither);
+	$('#coefficient').html(obj.coefficients[0].value);
 
-        var s = '<select onchange="$(\'#coefficient\').html($(this).val());" id="coefficients">';
+	var s = '<select onchange="$(\'#coefficient\').html($(this).val());" id="coefficients">';
 
-        for (i in obj.coefficients) {
-       
-                s = s + '<option value="'+obj.coefficients[i].value+'"'+(i==0 ? ' selected="selected"' : '')+'>'+obj.coefficients[i].name+'</option>'+"\n";
+	for (i in obj.coefficients) {
+	
+		s = s + '<option value="'+obj.coefficients[i].value+'"'+(i==0 ? ' selected="selected"' : '')+'>'+obj.coefficients[i].name+'</option>'+"\n";
 
-        }
-       
-        s = s + '</select>';
+	}
+	
+	s = s + '</select>';
 
 
-        $('#formula').html(s);
+	$('#formula').html(s);
 
-        $('#states').removeClass().addClass('visible');
+	$('#states').removeClass().addClass('visible');
 
 }
