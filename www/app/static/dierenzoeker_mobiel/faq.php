4fdf72876143f8c134558a3af034fa331b735f82
maarten schermer <maarten.schermer@naturalis.nl>
2014-12-19 16:07:54 +0100

    dierenzoeker new comscore codes

--- www/app/static/dierenzoeker_mobiel/faq.php
+++ www/app/static/dierenzoeker_mobiel/faq.php
@@ -50,6 +50,17 @@
 
 <a href="mailto:dierenzoeker@naturalis.nl?subject=Dierenzoeker vraag" data-role="button" id="askNaturalis" class="simplebutton ie-rounded ui-btn ui-btn-corner-all ui-shadow ui-btn-hover-c" style="background-color:#5a5c5f;color:white;border:none;" data-theme="c"><span class="ui-btn-inner ui-btn-corner-all" aria-hidden="true"><span class="ui-btn-text">Staat je vraag er niet tussen? <br>Mail Naturalis</span></span></a>						
 
+
+<!-- Begin comScore Inline Tag 1.1302.13 --> 
+<script type="text/javascript"> 
+// <![CDATA[
+function udm_(e){var t="comScore=",n=document,r=n.cookie,i="",s="indexOf",o="substring",u="length",a=2048,f,l="&ns_",c="&",h,p,d,v,m=window,g=m.encodeURIComponent||escape;if(r[s](t)+1)for(d=0,p=r.split(";"),v=p[u];d<v;d++)h=p[d][s](t),h+1&&(i=c+unescape(p[d][o](h+t[u])));e+=l+"_t="+ +(new Date)+l+"c="+(n.characterSet||n.defaultCharset||"")+"&c8="+g(n.title)+i+"&c7="+g(n.URL)+"&c9="+g(n.referrer),e[u]>a&&e[s](c)>0&&(f=e[o](0,a-8).lastIndexOf(c),e=(e[o](0,f)+l+"cut="+g(e[o](f+1)))[o](0,a)),n.images?(h=new Image,m.ns_p||(ns_p=h),h.src=e):n.write("<","p","><",'img src="',e,'" height="1" width="1" alt="*"',"><","/p",">")};udm_('http'+(document.location.href.charAt(4)=='s'?'s://sb':'://b')+'.scorecardresearch.com/b?c1=2&c2=17827132&ns_site=po-totaal&name=hetklokhuis.dierenzoeker.veelgesteldevragen.page&potag1=hetklokhuis&potag2=dierenzoeker&potag3=ntr&potag4=ntr&potag5=programma&potag6=video&potag7=npozapp&potag8=site&potag9=site&ntr_genre=jeugd');
+// ]]>
+</script>
+<noscript><p><img src="http://b.scorecardresearch.com/p?c1=2&amp;c2=17827132&amp;ns_site=po-totaal&amp;name=hetklokhuis.dierenzoeker.veelgesteldevragen.page&amp;potag1=hetklokhuis&amp;potag2=dierenzoeker&amp;potag3=ntr&amp;potag4=ntr&amp;potag5=programma&amp;potag6=video&amp;potag7=npozapp&amp;potag8=site&amp;potag9=site&amp;ntr_genre=jeugd" height="1" width="1" alt="*"></p></noscript> 
+<script type="text/javascript" language="JavaScript1.3" src="http://b.scorecardresearch.com/c2/17827132/cs.js"></script>
+<!-- End comScore Inline Tag -->
+
                 
 <script language="JavaScript">
 $(document).ready(function(e) {
@@ -76,11 +87,3 @@ $(document).ready(function(e) {
 
 </strong>    </div>
 </div>
-
-
-
-
-
-
-
-
