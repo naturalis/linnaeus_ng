f4eabd5b947848462bd0e41405f3478368bd2405
maarten schermer <maarten.schermer@naturalis.nl>
2015-03-13 10:23:47 +0100

    Fixes

--- configuration/app/controllers/SpeciesControllerNSR.php
+++ configuration/app/controllers/SpeciesControllerNSR.php
@@ -483,7 +483,11 @@ class SpeciesControllerNSR extends SpeciesController
 				$start=$val['id'];
 			} 
 			else
-			if (is_null($start) && !empty($order[$val['tabname']]['start_order']) && empty($val['is_empty']))
+			if (!empty($order[$val['tabname']]['start_order']) &&  empty($val['is_empty']) && 
+				(
+					is_null($start) || 
+					$order[$val['tabname']]['start_order']<$start
+				))
 			{
 				$start=$val['id'];
 			}
