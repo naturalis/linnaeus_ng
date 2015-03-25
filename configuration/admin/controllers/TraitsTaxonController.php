2dd7af5255cbab43d7e0f7a86eee5d6fa1e03ec8
maarten schermer <maarten.schermer@naturalis.nl>
2015-03-25 17:26:04 +0100

    Stuff

--- configuration/admin/controllers/TraitsTaxonController.php
+++ configuration/admin/controllers/TraitsTaxonController.php
@@ -15,7 +15,8 @@ class TraitsTaxonController extends TraitsController
 		'traits_taxon_values',
 		'traits_taxon_freevalues',
 		'traits_taxon_references',
-		'literature2'
+		'literature2',
+		'literature2_authors'
     );
    
     public $controllerPublicName = 'Kenmerken';
@@ -50,77 +51,26 @@ class TraitsTaxonController extends TraitsController
     {
     }
 	
-	private function saveTaxonTraitData($p)
-	{
-		$project=$this->getCurrentProjectId();
-		$taxon=isset($p['taxon']) ? $p['taxon'] : null;
-		$trait=isset($p['trait']) ? $p['trait'] : null;
-		$values=isset($p['values']) ? $p['values'] : null;
-
-		if (empty($project)||empty($taxon)||empty($trait))
-		{
-			return;
-		}
-
-		$existing=$this->getTaxonValues(array('taxon'=>$taxon,'trait'=>$trait));
-
-		if (isset($existing[0]['values']))
-		{
-			$delete=[];
-			foreach((array)$existing[0]['values'] as $key=>$val)
-			{
-				$exists=false;
-				foreach((array)$values as $newvalue)
-				{
-					if ($newvalue['id']!=-1 && $newvalue['id']==$val['id'])
-					{
-						$exists=true;
-					}
-				}
-				if (!$exists)
-				{
-					$delete[]=$val['id'];
-				}
-			}
-			
-			foreach((array)$delete as $val)
-			{
-				$this->models->TraitsTaxonValues->delete(array(
-					'project_id'=>$this->getCurrentProjectId(),
-					'id'=>$val
-				));
-			}
-		}
-		
-		foreach((array)$values as $val)
-		{
-			if ($val['id']!=-1) continue;
-
-			$this->models->TraitsTaxonValues->save(array(
-				'project_id'=>$this->getCurrentProjectId(),
-				'taxon_id'=>$taxon,
-				'value_id'=>$val['value_id']
-			));
-		}		
-	}
-
     public function taxonAction()
     {
 		$this->checkAuthorisation();
 		$this->setPageName($this->translate('Taxon trait data'));
+		
+//$this->isFormResubmit()
 		
 		if ($this->rHasVal('action','save') && $this->rHasId() && $this->rHasVal('group'))
 		{
 			$this->saveTaxonTraitData(array(
 				'taxon'=>$this->rGetId(),
 				'trait'=>$this->rGetVal('trait'),
-				'values'=>$this->rHasVal('values') ? array_map(function($a){ return json_decode(urldecode($a),true); },$this->requestData['values']) : null
+				'values'=>$this->rGetVal('values')
 			));
 			$this->addMessage('Saved');
 		}
 		
 		if ($this->rHasId('id') && $this->rHasVal('group'))
 		{
+			$this->smarty->assign('references',$this->getReferences(array('taxon'=>$this->rGetId(),'group'=>$this->rGetVal('group'))));
 			$this->smarty->assign('group',$this->getTraitgroup($this->rGetVal('group')));
 			$this->smarty->assign('traits',$this->getTraitgroupTraits($this->rGetVal('group')));
 			$this->smarty->assign('concept',$this->getTaxonById($this->rGetId()));
@@ -146,8 +96,6 @@ class TraitsTaxonController extends TraitsController
 
 		$this->printPage('ajax_interface');
     }
-
-
 
 
     private function getTaxonValues($p)
@@ -353,5 +301,147 @@ class TraitsTaxonController extends TraitsController
 		return $data;
 
 	}
+
+	private function saveTaxonTraitData($p)
+	{
+		$project=$this->getCurrentProjectId();
+		$taxon=isset($p['taxon']) ? $p['taxon'] : null;
+		$trait=isset($p['trait']) ? $p['trait'] : null;
+		$values=isset($p['values']) ? $p['values'] : null;
+
+		if (empty($project)||empty($taxon)||empty($trait))
+		{
+			return;
+		}
+
+		$existing=$this->getTaxonValues(array('taxon'=>$taxon,'trait'=>$trait));
+		$t=$this->getTraitgroupTrait(array('trait'=>$trait));
+
+		if ($existing)
+		{
+			$existing=$existing[0];
+		}
+		
+		if ($t['type_sysname']=='stringfree')
+		{
+			if (isset($existing['values'][0]['id']))
+			{
+				if (empty($values[0]))
+				{
+					$this->models->TraitsTaxonFreevalues->delete(array(
+						'project_id'=>$this->getCurrentProjectId(),
+						'id'=>$existing['values'][0]['id']
+					));
+				}
+				else
+				{
+					$this->models->TraitsTaxonFreevalues->save(array(
+						'project_id'=>$this->getCurrentProjectId(),
+						'id'=>$existing['values'][0]['id'],
+						'taxon_id'=>$taxon,
+						'trait_id'=>$trait,
+						'string_value'=>$values[0]
+					));
+				}
+			}
+			else
+			{
+				if (!empty($values[0]))
+				{
+					$this->models->TraitsTaxonFreevalues->save(array(
+						'project_id'=>$this->getCurrentProjectId(),
+						'taxon_id'=>$taxon,
+						'trait_id'=>$trait,
+						'string_value'=>$values[0]
+					));
+				}
+			}
+		}
+		else
+		if ($t['type_sysname']=='stringlist')
+		{
+			if (isset($existing['values']))
+			{
+				foreach((array)$existing['values'] as $key=>$val)
+				{
+					$this->models->TraitsTaxonValues->delete(array(
+						'project_id'=>$this->getCurrentProjectId(),
+						'id'=>$val['id']
+					));
+				}
+			}
+	
+			foreach((array)$values as $val)
+			{
+				$this->models->TraitsTaxonValues->save(array(
+					'project_id'=>$this->getCurrentProjectId(),
+					'taxon_id'=>$taxon,
+					'value_id'=>$val
+				));
+			}		
+		}
+	}
+
+	private function getReferences($p)
+	{
+		$taxon=isset($p['taxon']) ? $p['taxon'] : null;
+		$group=isset($p['group']) ? $p['group'] : null;
+		$project=$project=$this->getCurrentProjectId();
+
+		if (empty($taxon)||empty($group)||empty($project))
+			return;
+
+		$l=$this->models->Literature2->freeQuery("
+			select
+				_a.*,
+				_h.label as publishedin_label,
+				_i.label as periodical_label
+
+			from %PRE%literature2 _a
+
+			right join %PRE%traits_taxon_references _ttr
+				on _a.id = _ttr.reference_id 
+				and _a.project_id=_ttr.project_id
+				and _ttr.trait_group_id=".$group."
+
+			left join  %PRE%literature2 _h
+				on _a.publishedin_id = _h.id 
+				and _a.project_id=_h.project_id
+
+			left join %PRE%literature2 _i 
+				on _a.periodical_id = _i.id 
+				and _a.project_id=_i.project_id
+
+			where
+				_a.project_id=".$project." 
+				and _ttr.taxon_id=".$taxon
+		);
+		
+		foreach((array)$l as $key=>$val)
+		{
+			$authors=$this->models->Literature2Authors->freeQuery("
+				select
+					_a.actor_id, _b.name
+	
+				from %PRE%literature2_authors _a
+	
+				left join %PRE%actors _b
+					on _a.actor_id = _b.id 
+					and _a.project_id=_b.project_id
+	
+				where
+					_a.project_id = ".$project."
+					and _a.literature2_id =".$val['id']."
+				order by _b.name
+			");
+		
+			$l[$key]['authors']=$authors;
+			
+		}
+		
+		return $l;
+
+	}
+
 
 }
