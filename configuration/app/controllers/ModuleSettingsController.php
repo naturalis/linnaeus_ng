d0d6383db50a17cfd539a895eb775663e88a17be
maarten schermer <maarten.schermer@naturalis.nl>
2015-07-16 13:25:52 +0200

    ....

--- configuration/app/controllers/ModuleSettingsController.php
+++ configuration/app/controllers/ModuleSettingsController.php
@@ -1,5 +1,43 @@
 <?php
 
+/*
+
+drop table `module_settings`;
+
+create table `module_settings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `project_id` int(11) NOT NULL,
+  `module` varchar(64) NOT NULL,
+  `lng_id` int(11) NULL,
+  `item_type` varchar(64) NULL,
+  `setting` varchar(64) NOT NULL,
+  `value` varchar(255) NOT NULL,
+  `created` datetime NOT NULL,
+  `last_change` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `module_settings_1` (`project_id`),
+  KEY `module_settings_2` (`project_id`,`module`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
+;
+
+truncate module_settings ;
+insert into module_settings values
+(null,65,'matrixkey',null,null,'state_image_max_height',300,now(),now()),
+(null,65,'matrixkey',null,null,'browse_style','expand',now(),now()),
+(null,65,'matrixkey',null,null,'state_image_per_row',4,now(),now()),
+(null,65,'matrixkey',null,null,'items_per_page',16,now(),now()),
+(null,65,'matrixkey',null,null,'use_character_groups',1,now(),now()),
+(null,65,'matrixkey',null,null,'allow_empty_species',1,now(),now()),
+(null,65,'matrixkey',null,null,'calc_char_h_val',0,now(),now()),
+(null,65,'matrixkey',null,null,'use_emerging_characters',1,now(),now()),
+(null,65,'matrixkey',null,null,'score_threshold',100,now(),now()),
+(null,65,'matrixkey',null,null,'img_to_thumb_regexp_pattern',"/http:\\/\\/images.naturalis.nl\\/original\\//",now(),now()),
+(null,65,'matrixkey',null,null,'img_to_thumb_regexp_replacement',"http://images.naturalis.nl/comping/",now(),now())
+;
+
+
+*/
+
 include_once ('Controller.php');
 class ModuleSettingsController extends Controller
 {
