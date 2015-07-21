7c2f54a24336d6e8b106891c1f41df98692e564c
maarten schermer <maarten.schermer@naturalis.nl>
2015-07-21 13:28:56 +0200

    separated matrix templates

--- www/app/javascript/project_specific/dierendeterminatie.js
+++ www/app/javascript/project_specific/dierendeterminatie.js
@@ -5,143 +5,6 @@ function __(text)
 	return _(text);
 }
 
-var resultsHtmlTpl = '<div class="resultRow">%RESULTS%</div>';
-var noResultHtmlTpl='<div style="margin-top:10px">%MESSAGE%</div>';
-var resultsLineEndHtmlTpl = '</div><br/><div class="resultRow">';
-var brHtmlTpl = '<br />';
-
-var photoLabelHtmlTpl = ' \
-<div style="margin-left:130px"> \
-	%SCI-NAME% \
-	%GENDER% \
-	%COMMON-NAME% \
-	%PHOTO-DETAILS% \
-</div> \
-';
-
-//var photoLabelGenderHtmlTpl = '<img class="gender" height="17" width="8" src="%IMG-SRC%" title="%GENDER-LABEL%" />';
-var photoLabelGenderHtmlTpl = '';
-
-var photoLabelPhotographerHtmlTpl = '<br />(%PHOTO-LABEL% %PHOTOGRAPHER%)';
-
-var imageHtmlTpl = '\
-<a rel="prettyPhoto[gallery]" href="%IMAGE-URL%" pTitle="%PHOTO-LABEL%" title=""> \
-	<img class="result-image" src="%THUMB-URL%" title="%PHOTO-CREDIT%" /> \
-</a>\
-';
-
-//var genderHtmlTpl = '<img class="result-gender-icon" src="%ICON-URL%" title="%GENDER-LABEL%" />';
-var genderHtmlTpl = '';
-
-var matrixLinkHtmlTpl = '<br /><a href="?mtrx=%MATRIX-ID%">%MATRIX-LINK-TEXT%</a>';
-
-var remoteLinkClickHtmlTpl = 'onclick="window.open(\'%REMOTE-LINK%\',\'_blank\');" title="%TITLE%"';
-
-var statesClickHtmlTpl = 'onclick="toggleDetails(\'%LOCAL-ID%\');return false;"title="%TITLE%"';
-
-var relatedClickHtmlTpl = 'onclick="setSimilar({id:%ID%,type:\'%TYPE%\'});return false;" title="%TITLE%"';
-
-var statesHtmlTpl = '\
-<div id="det-%LOCAL-ID%" class="result-detail hidden"> \
-	<ul> \
-		<li>%STATES%</li> \
-	</ul> \
-</div> \
-';
-
-var statesJoinHtmlTpl = '</li><li>';
-
-var speciesStateItemHtmlTpl = '<span class="result-detail-label">%GROUP% %CHARACTER%:</span> <span class="result-detail-value">%STATE%</span>';
-
-var resultHtmlTpl = '\
-<div class="result%CLASS-HIGHLIGHT%" id="res-%LOCAL-ID%"> \
-	<div class="result-result"> \
-		<div class="result-image-container"> \
-			%IMAGE-HTML% \
-		</div> \
-		<div class="result-labels"> \
-			%GENDER% \
-			<span class="result-name-scientific" title="%SCI-NAME-TITLE%">%SCI-NAME%</span> \
-			%MATRIX-LINK% \
-			<span class="result-name-common" title="%COMMON-NAME-TITLE%"><br />%COMMON-NAME%</span> \
-            </div> \
-        </div> \
-        <div class="result-icons"> \
-			<div class="result-icon%REMOTE-LINK-CLASS%" \
-				%REMOTE-LINK-CLICK% \
-			>%REMOTE-LINK-ICON%</div> \
-			<div class="result-icon%SHOW-STATES-CLASS%" id="tog-%LOCAL-ID%" \
-				%SHOW-STATES-CLICK% \
-			>%SHOW-STATES-ICON%</div> \
-			<div class="result-icon%RELATED-CLASS% related" \
-				%RELATED-CLICK% \
-			>%RELATED-ICON%</div> \
-        </div>%STATES% \
-    </div> \
-';
-
-var resultBatchHtmlTpl= '<span class=result-batch style="%STYLE%">%RESULTS%</span>' ;
-var buttonMoreHtmlTpl='<li id="show-more"><input type="button" id="show-more-button" onclick="printResults();return false;" value="%LABEL%" class="ui-button"></li>';
-var counterExpandHtmlTpl='%START-NUMBER%%NUMBER-SHOWING%&nbsp;%FROM-LABEL%&nbsp;%NUMBER-TOTAL%';
-var pagePrevHtmlTpl='<li><a href="#" onclick="browsePage(\'p\');return false;">&lt;</a></li>';
-var pageCurrHtmlTpl='<li><strong>%NR%</strong></li>';
-var pageNumberHtmlTpl='<li><a href="#" onclick="browsePage(%INDEX%);return false;">%NR%</a></li>';
-var pageNextHtmlTpl='<li><a href="#" onclick="browsePage(\'n\');return false;" class="last">&gt;</a></li>';
-var counterPaginateHtmlTpl=' %FIRST-NUMBER%-%LAST-NUMBER% %NUMBER-LABEL% %NUMBER-TOTAL%';
-
-var menuOuterHtmlTpl ='<ul>%MENU%</ul>';
-
-var menuGroupHtmlTpl = '\
-<li id="character-item-%ID%" class="closed"><a href="#" onclick="toggleGroup(%ID%);return false;">%LABEL%</a></li> \
-<ul id="character-group-%ID%" class="hidden"> \
-	%CHARACTERS% \
-</ul> \
-';
-
-var menuLoneCharHtmlTpl='\
-<li class="inner ungrouped last"> \
-	<a class="facetLink" href="#" onclick="showStates(%ID%);return false;">%LABEL%%VALUE%</a> \
-	%SELECTED% \
-</li> \
-';
-var menuLoneCharDisabledHtmlTpl='<li class="inner ungrouped %CLASS% disabled" title="%TITLE%" ondblclick="showStates(%ID%);">%LABEL%%VALUE%	%SELECTED% </li>';
-var menuLoneCharEmergentDisabledHtmlTpl='\
-<li class="inner ungrouped %CLASS%" title="%TITLE%"> \
-	<a class="facetLink emergent_disabled" href="#" onclick="showStates(%ID%);return false;">(%LABEL%%VALUE%)</a> \
-	%SELECTED% \
-</li> \
-';
-var menuCharHtmlTpl=menuLoneCharHtmlTpl.replace('ungrouped ','');
-var menuCharDisabledHtmlTpl=menuLoneCharDisabledHtmlTpl.replace('ungrouped ','');
-var menuCharEmergentDisabledHtmlTpl=menuLoneCharEmergentDisabledHtmlTpl.replace('ungrouped ','');
-
-var menuSelStateHtmlTpl = '\
-<div class="facetValueHolder"> \
-%VALUE% %LABEL% %COEFF% \
-<a href="#" class="removeBtn" onclick="clearStateValue(\'%STATE-ID%\');return false;"> \
-<img src="%IMG-URL%"></a> \
-</div> \
-';
-var menuSelStatesHtmlTpl = '<span>%STATES%</span>';
-
-var iconInfoHtmlTpl='<img class="result-icon-image icon-info" src="%IMG-URL%">';
-var iconUrlHtmlTpl = iconInfoHtmlTpl.replace(' icon-info','');
-var iconSimilarTpl = iconInfoHtmlTpl.replace(' icon-info',' icon-similar');
-
-var similarHeaderHtmlTpl='\
-%HEADER-TEXT% <span id="similarSpeciesName">%SPECIES-NAME%</span> <span class="result-count">(%NUMBER-START%-%NUMBER-END%)</span> \
-<br /> \
-<a class="clearSimilarSelection" href="#" onclick="closeSimilar();return false;">%BACK-TEXT%</a> \
-<span id="show-all-divider"> | </span> \
-<a class="clearSimilarSelection" href="#" onclick="toggleAllDetails();return false;" id="showAllLabel">%SHOW-STATES-TEXT%</a> \
-';
-
-var searchHeaderHtmlTpl='\
-%HEADER-TEXT% <span id="similarSpeciesName">%SEARCH-TERM%</span> <span class="result-count">(%NUMBER-START%-%NUMBER-END% %OF-TEXT% %NUMBER-TOTAL%)</span> \
-<br /> \
-<a class="clearSimilarSelection" href="#" onclick="closeSearch();return false;">%BACK-TEXT%</a> \
-';
-
 var settings={
 	matrixId: 0,
 	projectId: 0,
@@ -1558,6 +1421,21 @@ function getCharacter( id )
 	}
 }
 
+function disableImgContextMenu()
+{
+    $("img").on("contextmenu",function(){
+       return false;
+    });
+}
+
+function showRestartButton()
+{
+	$('#clearSelectionContainer').removeClass('ghosted');
+}
+
+
+
+
 function matrixInit()
 {
 	nbcLabelClose = __('sluiten');
@@ -1583,19 +1461,4 @@ function matrixInit()
 		// "desktop" code
 	}
 	*/
-
-}
-
-function disableImgContextMenu()
-{
-
-    $("img").on("contextmenu",function(){
-       return false;
-    });
-
-}
-
-function showRestartButton()
-{
-	$('#clearSelectionContainer').removeClass('ghosted');
 }
