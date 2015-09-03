28d15ab6938a277c9db83c1c3aba25faedd3000a
maarten schermer <maarten.schermer@naturalis.nl>
2015-09-03 14:20:14 +0200

    more fixing!

--- configuration/app/controllers/MatrixKeyController.php
+++ configuration/app/controllers/MatrixKeyController.php
@@ -146,8 +146,23 @@ class MatrixKeyController extends Controller
 		$this->smarty->assign('session_states',json_encode( $this->getSessionStates() ));
 		$this->smarty->assign('session_characters',json_encode( $this->getCharacterCounts() ));
 		$this->smarty->assign('session_statecount',json_encode( $this->setRemainingStateCount() ));
-		$this->smarty->assign('full_dataset',json_encode( $this->getDataSet() ));
 
+		//$this->smarty->assign('full_dataset',json_encode( $this->getDataSet() ));
+		/* 
+			first page of dataset is loaded directly with the page. subsequently, the full dataset
+			is loaded by ajax. loading all of the resultset with the page has in the past lead to
+			memory problems (key of over 100 species on an ipad). the merging with an empty array
+			to fill out again to the size of the actual dataset is done to fool the result counters
+			in the html-page.
+		*/
+		$this->smarty->assign('limited_dataset',
+			json_encode(
+				array_merge(
+					array_slice($this->getDataSet(),0,$this->settings->items_per_page),
+					array_fill($this->settings->items_per_page,count((array)$this->getDataSet())-$this->settings->items_per_page,null)
+				)
+			)
+		);
         $this->smarty->assign('matrix', $matrix);
 		$this->smarty->assign('master_matrix', $this->getMasterMatrix() );
 		$this->smarty->assign('facetmenu', $this->getFacetMenu());
