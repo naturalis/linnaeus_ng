b5caba8c19989eaf1e726fe0fb9c7380cbf9bc44
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2015-11-27 16:27:49 +0100

    jaja

--- tools/lit_to_lit2.php
+++ tools/lit_to_lit2.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>Update authors</title>
+	<title>Lit to Lit2</title>
 </head>
 <body style="font: 12px Verdana; width: 800px;">
 
