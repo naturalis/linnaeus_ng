b27e1f9795e501c46bba9f248f9cb19c507a20d9
maarten schermer <maarten.schermer@naturalis.nl>
2015-11-30 09:56:46 +0100

    this

--- configuration/app/controllers/MatrixKeyAppController.php
+++ configuration/app/controllers/MatrixKeyAppController.php
@@ -86,34 +86,12 @@ class MatrixKeyAppController extends Controller
 		$selStates=isset($data['states']) ? preg_split('/,/',$data['states'],-1,PREG_SPLIT_NO_EMPTY) : array();
 		$stateList=array();
 
-		$count = $this->models->MatrixTaxonState->freeQuery(
-			array(
-				'query' =>
-			"select case count(1) when 0 then -1 else 0 end as can_select, state_id
-			from %TABLE%
-			where project_id = ".$this->getCurrentProjectId()." and matrix_id = ".$data['matrix'].
-			(count($resTaxa)!=0 ? " and taxon_id in (".implode(',',$resTaxa).") " : "" ).
-			(count($selStates)!=0 ? " and state_id not in (".$data['states'].") " : "")."
-			group by state_id
-				union all
-			select distinct -1 as can_select, state_id
-			from %TABLE%
-			where project_id = ".$this->getCurrentProjectId()." and matrix_id = ".$data['matrix']." 
-			".(count($resTaxa)!=0 ? " and taxon_id not in (".implode(',',$resTaxa).") " : "" ). "
-			and state_id not in (
-				select state_id from %TABLE%
-				where project_id = ".$this->getCurrentProjectId()." and matrix_id = ".$data['matrix'].
-				(count($resTaxa)!=0 ? " and taxon_id in (".implode(',',$resTaxa).") " : "" ).
-				(count($selStates)!=0 ? " and state_id not in (".$data['states'].") " : "").
-			")
-				union all
-			select 1 as can_select, id as state_id
-			from %PRE%characteristics_states
-			where project_id = ".$this->getCurrentProjectId()." 
-			and id in (".(count($selStates)!=0 ?  $data['states'] : '-1' ).") ",
-				'fieldAsIndex' => 'state_id'
-			)
-		);
+		$count = $this->models->MatrixKeyAppModel->getStateCount(array(
+			"project_id = ".$this->getCurrentProjectId(),
+			"matrix_id = ".$data['matrix'],
+			"taxa"=>$resTaxa,
+			"states"=>$data['states']
+		));
 
 		$menu=$this->getGuiMenuOrder($data['matrix'],$data['language']);
 
@@ -125,28 +103,11 @@ class MatrixKeyAppController extends Controller
 
 			if ($val['type']=='character')
 			{
-
-				//$menu[$key]['img']=makeCharacterIconName($val['label']);
-	
-				$menu[$key]['states']=$this->models->CharacteristicState->freeQuery(
-					"select
-						_a.id,
-						_c.label,
-						_a.file_name as img,
-						'0' as select_state, 
-						_a.show_order
-					from %TABLE% _a
-					left join %PRE%characteristics_labels_states _c 
-						on _a.id = _c.state_id 
-						and _c.language_id = ".$data['language']." 
-						and _c.project_id = ".$this->getCurrentProjectId()."
-					where
-						_a.characteristic_id = ".$val['id']." 
-						and _a.project_id = ".$this->getCurrentProjectId()."
-					order by 
-						_a.show_order,
-						_c.label"
-				);
+				$menu[$key]['states']=$this->models->MatrixKeyAppModel->getCharacteristicStates(array(
+					"language_id"=>$data['language'],
+					"project_id"=>$this->getCurrentProjectId(),
+					"characteristic_id"=>$val['id']
+				));
 
 				$hasSelected=false;
 				foreach((array)$menu[$key]['states'] as $sKey => $sVal)
@@ -274,163 +235,22 @@ class MatrixKeyAppController extends Controller
 		return array('all'=>$menu,'active'=>$stateList);
 	}
 
-	private function _appControllerGetResults($data)
+	private function _appControllerGetResults( $data )
 	{
-		
-		$selStateCount=count(isset($data['states']) ? preg_split('/,/', $data['states'],-1,PREG_SPLIT_NO_EMPTY) : array());
-
-		if ($selStateCount==0) {
-			
-			$res=$this->models->MatrixTaxon->freeQuery("
-				select 'taxon' as type, _a.taxon_id as id, 0 as total_states, 100 as score,_c.is_hybrid as is_hybrid, trim(_c.taxon) as sci_name, trim(_d.commonname) as label,_e.value as url_thumbnail
-				from %TABLE% _a
-				left join %PRE%taxa _c on _a.taxon_id = _c.id and _c.project_id = ".$this->getCurrentProjectId()."
-				left join %PRE%commonnames _d on _d.taxon_id = _a.taxon_id and _d.project_id = ".$this->getCurrentProjectId()." and _d.language_id = ".$data['language']." 
-				left join %PRE%nbc_extras _e on _c.id = _e.ref_id and _e.ref_type='taxon' and _e.name='url_thumbnail' and _e.project_id = ".$this->getCurrentProjectId()."
-				where _a.project_id = ".$this->getCurrentProjectId()."
-				group by _a.taxon_id
-				union all
-				select 'variation' as type, _a.variation_id as id, 0 as total_states, 100 as score,0 as is_hybrid, trim(_d.taxon) as sci_name, trim(_c.label) as label, _e.value as url_thumbnail
-				from  %PRE%matrices_variations _a
-				left join %PRE%taxa_variations _c on _a.variation_id = _c.id and _c.project_id = ".$this->getCurrentProjectId()."
-				left join %PRE%taxa _d on _c.taxon_id = _d.id and _d.project_id = ".$this->getCurrentProjectId()."
-				left join %PRE%nbc_extras _e on _a.variation_id = _e.ref_id and _e.ref_type='variation' and _e.name='url_thumbnail' and _e.project_id = ".$this->getCurrentProjectId()."
-				where _a.project_id = ".$this->getCurrentProjectId()."
-				group by _a.variation_id
-				order by label"
-			);
-
-		} else {
-
-			$res=$this->models->MatrixTaxon->freeQuery("
-				select 'taxon' as type, _a.taxon_id as id, count(_b.state_id) as total_states,
-				round((case when count(_b.state_id)>".$selStateCount." then ".$selStateCount." else count(_b.state_id) end/".$selStateCount.")*100,0) as score,
-				_c.is_hybrid as is_hybrid, trim(_c.taxon) as sci_name, trim(_d.commonname) as label,_e.value as url_thumbnail
-				from %TABLE% _a
-				left join %PRE%matrices_taxa_states _b on _a.project_id = _b.project_id and _a.matrix_id = _b.matrix_id and _a.taxon_id = _b.taxon_id and (_b.state_id in (".$data['states'].")) and _b.project_id = ".$this->getCurrentProjectId()."
-				left join %PRE%taxa _c on _a.taxon_id = _c.id  and _c.project_id = ".$this->getCurrentProjectId()."
-				left join %PRE%commonnames _d on _d.taxon_id = _a.taxon_id and _d.project_id = ".$this->getCurrentProjectId()." and _d.language_id = ".$data['language']." 
-				left join %PRE%nbc_extras _e on _c.id = _e.ref_id and _e.ref_type='taxon' and _e.name='url_thumbnail' and _e.project_id = ".$this->getCurrentProjectId()."
-				where _a.project_id = ".$this->getCurrentProjectId()."
-				group by _a.taxon_id having score=100
-				union all
-				select 'variation' as type, _a.variation_id as id, count(_b.state_id) as total_states,
-				round((case when count(_b.state_id)>".$selStateCount." then ".$selStateCount." else count(_b.state_id) end/".$selStateCount.")*100,0) as score,
-				0 as is_hybrid, trim(_d.taxon) as sci_name, trim(_c.label) as label, _e.value as url_thumbnail
-				from  %PRE%matrices_variations _a
-				left join %PRE%matrices_taxa_states _b on _a.project_id = _b.project_id and _a.matrix_id = _b.matrix_id and _a.variation_id = _b.variation_id and (_b.state_id in (".$data['states'].")) and _b.project_id = ".$this->getCurrentProjectId()."
-				left join %PRE%taxa_variations _c on _a.variation_id = _c.id and _c.project_id = ".$this->getCurrentProjectId()."
-				left join %PRE%taxa _d on _c.taxon_id = _d.id and _d.project_id = ".$this->getCurrentProjectId()."
-				left join %PRE%nbc_extras _e on _a.variation_id = _e.ref_id and _e.ref_type='variation' and _e.name='url_thumbnail' and _e.project_id = ".$this->getCurrentProjectId()."
-				where _a.project_id = ".$this->getCurrentProjectId()."
-				group by _a.variation_id having score=100
-				order by score,label"
-			);
-		}
-		
-		return $res;
-					
+		return 
+			$this->models->MatrixKeyAppModel->getResults(array(
+				"project_id"=>$this->getCurrentProjectId(),
+				"language_id"=>$data['language'],
+				"states"=>isset($data['states']) ? $data['states'] : null,
+			));					
 	}
 			
 	private function _appControllerGetDetail($data)
 	{
-
-		$t=$this->models->Taxon->freeQuery("
-			select t.id,trim(replace(t.taxon,'%VAR%','')) as name_sci, c.commonname as name_nl, 
-				p.id as group_id, p.taxon as groupname_sci, pc.commonname as groupname_nl, 'taxon' as type 
-			from %PRE%taxa t
-			left join %PRE%commonnames c 
-				on c.taxon_id = t.id 
-				and c.project_id = ".$this->getCurrentProjectId()."
-			left join %PRE%taxa p 
-				on t.parent_id = p.id 
-				and p.project_id = ".$this->getCurrentProjectId()."
-			left join %PRE%commonnames pc 
-				on pc.taxon_id = p.id 
-				and pc.project_id = ".$this->getCurrentProjectId()."
-			where t.id = ".$data['id']."
-			and t.project_id = ".$this->getCurrentProjectId()
-		);
-		$res=$t[0];
-
-
-		$t=$this->models->Taxon->freeQuery("
-			select _b.title,_a.content, _c.page 
-			from %PRE%content_taxa _a
-			left join %PRE%pages_taxa_titles _b 
-				on _a.page_id = _b.page_id 
-				and _b.project_id = ".$this->getCurrentProjectId()."
-			left join %PRE%pages_taxa _c 
-				on _a.page_id = _c.id 
-				and _c.project_id = ".$this->getCurrentProjectId()."
-			where _a.taxon_id = ".$data['id']."
-			and _a.project_id = ".$this->getCurrentProjectId()
-		);
-		$res['content']=$t;
-
-
-		$t=$this->models->Taxon->freeQuery("
-			select _a.value as file_name,_b.value as copyright, '1' as overview_image 
-			from %PRE%nbc_extras _a
-			left join %PRE%nbc_extras _b 
-				on _b.ref_type = 'taxon' 
-				and _b.ref_id=_a.ref_id 
-				and _b.name='photographer' 
-				and _b.project_id = ".$this->getCurrentProjectId()."
-			where _a.ref_id = ".$data['id']." 
-				and _a.ref_type='taxon' 
-				and _a.name='url_image'
-			and _a.project_id = ".$this->getCurrentProjectId()
-		);
-		$res['img_main']=$t;
-
-
-		$t=$this->models->Taxon->freeQuery("
-			select file_name from %PRE%media_taxon where taxon_id = ".$data['id']." and project_id = ".$this->getCurrentProjectId()
-		);
-		$res['img_other']=$t;
-
-
-		$t=$this->models->Taxon->freeQuery("
-			select 'taxon' as type, _b.id as id, _b.taxon as taxon,_c.commonname as label, _n.value as img 
-			from %PRE%taxa_relations _a 
-			left join %PRE%taxa _b 
-				on _b.id = _a.relation_id 
-				and _b.project_id = ".$this->getCurrentProjectId()."
-			left join %PRE%commonnames _c 
-				on _c.taxon_id = _b.id 
-				and _c.project_id = ".$this->getCurrentProjectId()."
-			left join %PRE%nbc_extras _n 
-				on _b.id = _n.ref_id 
-				and _n.ref_type='taxon' 
-				and _n.name='url_thumbnail' 
-				and _n.project_id = ".$this->getCurrentProjectId()."
-			where _a.ref_type='taxon' and _a.taxon_id = ".$data['id']." 
-			and _a.project_id = ".$this->getCurrentProjectId()."
-			union all
-			select 'variation' as type, _e.id as id,  _f.taxon as taxon, _e.label as label, _n.value as img 
-			from %PRE%taxa_relations _d 
-			left join %PRE%taxa_variations _e 
-				on _e.id = _d.relation_id 
-				and _e.project_id = ".$this->getCurrentProjectId()."
-			left join %PRE%taxa _f 
-				on _f.id = _d.taxon_id 
-				and _f.project_id = ".$this->getCurrentProjectId()."
-			left join %PRE%nbc_extras _n 
-				on _d.taxon_id = _n.ref_id 
-				and _n.ref_type='taxon' 
-				and _n.name='url_thumbnail' 
-				and _n.project_id = ".$this->getCurrentProjectId()."
-			where _d.ref_type='variation'  
-			and _d.taxon_id =".$data['id']." 
-			and _d.project_id = ".$this->getCurrentProjectId()
-		);
-		$res['similar']=$t;
-
-
-
-
-
+		$res=$this->models->MatrixKeyAppModel->getDetail(array(
+			"project_id"=>$this->getCurrentProjectId(),
+			"taxon_id"=>$data['id']
+		));
 
 		$t=$this->models->Taxon->_get(array('id'=>array('project_id' => $this->getCurrentProjectId(),'parent_id'=>$data['id'])));
 		foreach((array)$t as $key => $val)
--- configuration/app/models/MatrixKeyAppModel.php
+++ configuration/app/models/MatrixKeyAppModel.php
@@ -28,120 +28,366 @@ final class MatrixKeyAppModel extends AbstractModel
     }
 
 
+
+	private function getStateCount( $params )
+	{
+		$project_id = isset($params['project_id']) ? $params['project_id'] : null;
+		$matrix_id = isset($params['matrix_id']) ? $params['matrix_id'] : null;
+		$states = isset($params['states']) ? $params['states'] : null;
+		$taxa = isset($params['taxa']) ? $params['taxa'] : null;
+					
+		if ( is_null($project_id)  ||  is_null($matrix_id) )
+			return;
+			
+		$selStates=isset($data['states']) ? preg_split('/,/',$data['states'],-1,PREG_SPLIT_NO_EMPTY) : array();
+
+		$query="
+			select case count(1) when 0 then -1 else 0 end as can_select, state_id
+			from %TABLE%
+			where project_id = ".$project_id." and matrix_id = ".$matrix_id.
+			(count($taxa)!=0 ? " and taxon_id in (".implode(',',$taxa).") " : "" ).
+			(count($selStates)!=0 ? " and state_id not in (".$states.") " : "")."
+			group by state_id
+				union all
+			select distinct -1 as can_select, state_id
+			from %TABLE%
+			where project_id = ".$project_id." and matrix_id = ".$matrix_id." 
+			".(count($taxa)!=0 ? " and taxon_id not in (".implode(',',$taxa).") " : "" ). "
+			and state_id not in (
+				select state_id from %TABLE%
+				where project_id = ".$project_id." and matrix_id = ".$matrix_id.
+				(count($taxa)!=0 ? " and taxon_id in (".implode(',',$taxa).") " : "" ).
+				(count($selStates)!=0 ? " and state_id not in (".$states.") " : "").
+			")
+				union all
+			select 1 as can_select, id as state_id
+			from %PRE%characteristics_states
+			where project_id = ".$project_id." 
+			and id in (".(count($selStates)!=0 ?  $states : '-1' ).")"
+		;
+
+		return $this->freeQuery(array(
+			'query' => $query,
+			'fieldAsIndex' => 'state_id'
+		));
+	}
+
+
+
+	private function getCharacteristicStates( $params )
+	{
+		$project_id = isset($params['project_id']) ? $params['project_id'] : null;
+		$language_id = isset($params['language_id']) ? $params['language_id'] : null;
+		$characteristic_id = isset($params['characteristic_id']) ? $params['characteristic_id'] : null;
+					
+		if ( is_null($project_id) || is_null($language_id) || is_null($characteristic_id) )
+			return;
+			
+		$query="
+			select
+				_a.id,
+				_c.label,
+				_a.file_name as img,
+				'0' as select_state, 
+				_a.show_order
+			from %TABLE% _a
+			left join %PRE%characteristics_labels_states _c 
+				on _a.id = _c.state_id 
+				and _c.language_id = ".$language_id." 
+				and _c.project_id = ".$project_id."
+			where
+				_a.characteristic_id = ".$characteristic_id." 
+				and _a.project_id = ".$project_id."
+			order by 
+				_a.show_order,
+				_c.label"
+		;
+		
+		return $this->freeQuery( $query );
+	}
+
+	private function getCharacteristicStates( $params )
+	{
+		$project_id = isset($params['project_id']) ? $params['project_id'] : null;
+		$language_id = isset($params['language_id']) ? $params['language_id'] : null;
+		$characteristic_id = isset($params['characteristic_id']) ? $params['characteristic_id'] : null;
+					
+		if ( is_null($project_id) || is_null($language_id) || is_null($characteristic_id) )
+			return;
+			
+		$query="
+			select
+				_a.characteristic_id as id,
+				'character' as type,
+				_a.show_order as show_order,
+				if(locate('|',_c.label)=0,_c.label,substring(_c.label,1,locate('|',_c.label)-1)) as label,
+				if(locate('|',_c.label)=0,_c.label,substring(_c.label,locate('|',_c.label)+1)) as description					
+			from %TABLE% _a
+			left join %PRE%characteristics _b 
+				on _a.characteristic_id = _b.id
+			left join %PRE%characteristics_labels _c 
+				on _a.characteristic_id = _c.characteristic_id 
+				and _c.language_id = ".$language_id." 
+				and _c.project_id = ".$project_id."
+			where
+				_a.chargroup_id = ".$val['id']. " 
+				and _a.project_id = ".$project_id."
+			order by 
+				label"
+		;
+
+				$hasSelectedGroup=false;
+
+				foreach((array)$c as $cKey=>$cVal)
+				{
+					
+					$c[$cKey]['img']=makeCharacterIconName($val['label']);
+
+					$c[$cKey]['states']=$this->models->CharacteristicState->freeQuery(
+						"select
+							_a.id,
+							_c.label,
+							_a.file_name as img,
+							'0' as select_state, 
+							_a.show_order
+						from %TABLE% _a
+						left join %PRE%characteristics_labels_states _c 
+							on _a.id = _c.state_id 
+							and _c.language_id = ".$language_id." 
+							and _c.project_id = ".$project_id."
+						where 
+							_a.characteristic_id = ".$cVal['id']." 
+							and _a.project_id = ".$project_id."
+						order by 
+							_a.show_order,_c.label"
+					);
+					
+					$hasSelected=false;
+					foreach((array)$c[$cKey]['states'] as $sKey => $sVal)
+					{
+						unset($c[$cKey]['states'][$sKey]['show_order']);
+
+						$c[$cKey]['states'][$sKey]['select_state']=
+							isset($count[$sVal['id']]['can_select']) ? $count[$sVal['id']]['can_select'] : 0;
+
+						if ($c[$cKey]['states'][$sKey]['select_state']=='1') $hasSelected=true;
+
+						if (isset($data['force']) && $data['force']=='1' && empty($sVal['img']))
+							unset($c[$cKey]['states'][$sKey]);
+
+						if (in_array($sVal['id'],$selStates))
+						{
+							$stateList[]=
+								array_merge(
+									$sVal,
+									array(
+										'display'=>array_search($sVal['id'],$selStates),
+										'character'=>
+											array('id'=>$val['id'],'label'=>$cVal['label'])
+										)
+									);
+						}
+					}
+					
+					$c[$cKey]['hasStates']=count((array)$c[$cKey]['states'])>0;
+					$c[$cKey]['hasSelected']=$hasSelected;
+					
+					if ($hasSelected) $hasSelectedGroup=true;
+
+				}
+				
+				$menu[$key]['characters']=$c;
+				$menu[$key]['hasCharacters']=count((array)$c)>0;
+				$menu[$key]['hasSelected']=$hasSelectedGroup;
+				
+			}
+		
+		}
+		usort($stateList,function ($a,$b) {return $a['display']>$b['display'] ? 1 : -1;});
+
+		return array('all'=>$menu,'active'=>$stateList);
+	}
+
+
+
+
+
+
+
+	public function getResults( $params )
+	{
+		$project_id = isset($params['project_id']) ? $params['project_id'] : null;
+		$language_id = isset($params['language_id']) ? $params['language_id'] : null;
+		$states = isset($params['states']) ? $params['states'] : null;
+		
+		if ( is_null($project_id)  ||  is_null($language_id) )
+			return;
+
+		$selStateCount=count(isset($states) ? preg_split('/,/', $states,-1,PREG_SPLIT_NO_EMPTY) : array());
+	
+		if ($selStateCount==0)
+		{
+			$query="
+				select 'taxon' as type, _a.taxon_id as id, 0 as total_states, 100 as score,_c.is_hybrid as is_hybrid, trim(_c.taxon) as sci_name, trim(_d.commonname) as label,_e.value as url_thumbnail
+				from %TABLE% _a
+				left join %PRE%taxa _c on _a.taxon_id = _c.id and _c.project_id = ".$project_id."
+				left join %PRE%commonnames _d on _d.taxon_id = _a.taxon_id and _d.project_id = ".$project_id." and _d.language_id = ".$language_id." 
+				left join %PRE%nbc_extras _e on _c.id = _e.ref_id and _e.ref_type='taxon' and _e.name='url_thumbnail' and _e.project_id = ".$project_id."
+				where _a.project_id = ".$project_id."
+				group by _a.taxon_id
+				union all
+				select 'variation' as type, _a.variation_id as id, 0 as total_states, 100 as score,0 as is_hybrid, trim(_d.taxon) as sci_name, trim(_c.label) as label, _e.value as url_thumbnail
+				from  %PRE%matrices_variations _a
+				left join %PRE%taxa_variations _c on _a.variation_id = _c.id and _c.project_id = ".$project_id."
+				left join %PRE%taxa _d on _c.taxon_id = _d.id and _d.project_id = ".$project_id."
+				left join %PRE%nbc_extras _e on _a.variation_id = _e.ref_id and _e.ref_type='variation' and _e.name='url_thumbnail' and _e.project_id = ".$project_id."
+				where _a.project_id = ".$project_id."
+				group by _a.variation_id
+				order by label"
+			;
+
+		} 
+		else
+		{
+			$query="
+				select 'taxon' as type, _a.taxon_id as id, count(_b.state_id) as total_states,
+				round((case when count(_b.state_id)>".$selStateCount." then ".$selStateCount." else count(_b.state_id) end/".$selStateCount.")*100,0) as score,
+				_c.is_hybrid as is_hybrid, trim(_c.taxon) as sci_name, trim(_d.commonname) as label,_e.value as url_thumbnail
+				from %TABLE% _a
+				left join %PRE%matrices_taxa_states _b on _a.project_id = _b.project_id and _a.matrix_id = _b.matrix_id and _a.taxon_id = _b.taxon_id and (_b.state_id in (".$states.")) and _b.project_id = ".$project_id."
+				left join %PRE%taxa _c on _a.taxon_id = _c.id  and _c.project_id = ".$project_id."
+				left join %PRE%commonnames _d on _d.taxon_id = _a.taxon_id and _d.project_id = ".$project_id." and _d.language_id = ".$language_id." 
+				left join %PRE%nbc_extras _e on _c.id = _e.ref_id and _e.ref_type='taxon' and _e.name='url_thumbnail' and _e.project_id = ".$project_id."
+				where _a.project_id = ".$project_id."
+				group by _a.taxon_id having score=100
+				union all
+				select 'variation' as type, _a.variation_id as id, count(_b.state_id) as total_states,
+				round((case when count(_b.state_id)>".$selStateCount." then ".$selStateCount." else count(_b.state_id) end/".$selStateCount.")*100,0) as score,
+				0 as is_hybrid, trim(_d.taxon) as sci_name, trim(_c.label) as label, _e.value as url_thumbnail
+				from  %PRE%matrices_variations _a
+				left join %PRE%matrices_taxa_states _b on _a.project_id = _b.project_id and _a.matrix_id = _b.matrix_id and _a.variation_id = _b.variation_id and (_b.state_id in (".$states.")) and _b.project_id = ".$project_id."
+				left join %PRE%taxa_variations _c on _a.variation_id = _c.id and _c.project_id = ".$project_id."
+				left join %PRE%taxa _d on _c.taxon_id = _d.id and _d.project_id = ".$project_id."
+				left join %PRE%nbc_extras _e on _a.variation_id = _e.ref_id and _e.ref_type='variation' and _e.name='url_thumbnail' and _e.project_id = ".$project_id."
+				where _a.project_id = ".$project_id."
+				group by _a.variation_id having score=100
+				order by score,label"
+			;
+		}
+		
+		return $this->freeQuery( $query );
+					
+	}
+
 	public function getDetail( $params )
 	{
+		$project_id = isset($params['project_id']) ? $params['project_id'] : null;
+		$taxon_id = isset($params['taxon_id']) ? $params['taxon_id'] : null;
+		
+		if ( is_null($project_id)  ||  is_null($taxon_id) )
+			return;
 
-project_id	$this->getCurrentProjectId()
-taxon_id	$data['id']
+		$res=array();
 	
-		$t=$this->models->Taxon->freeQuery("
+		$query="
 			select t.id,trim(replace(t.taxon,'%VAR%','')) as name_sci, c.commonname as name_nl, 
 				p.id as group_id, p.taxon as groupname_sci, pc.commonname as groupname_nl, 'taxon' as type 
 			from %PRE%taxa t
 			left join %PRE%commonnames c 
 				on c.taxon_id = t.id 
-				and c.project_id = ".$this->getCurrentProjectId()."
+				and c.project_id = ".$project_id."
 			left join %PRE%taxa p 
 				on t.parent_id = p.id 
-				and p.project_id = ".$this->getCurrentProjectId()."
+				and p.project_id = ".$project_id."
 			left join %PRE%commonnames pc 
 				on pc.taxon_id = p.id 
-				and pc.project_id = ".$this->getCurrentProjectId()."
-			where t.id = ".$data['id']."
-			and t.project_id = ".$this->getCurrentProjectId()
-		);
-		$res=$t[0];
+				and pc.project_id = ".$project_id."
+			where t.id = ".$taxon_id."
+			and t.project_id = ".$project_id
+		;
 
+		$d=$this->freeQuery( $query );
 
-		$t=$this->models->Taxon->freeQuery("
+		if (!$d) return;
+
+		$res=$d[0];
+
+		$query="
 			select _b.title,_a.content, _c.page 
 			from %PRE%content_taxa _a
 			left join %PRE%pages_taxa_titles _b 
 				on _a.page_id = _b.page_id 
-				and _b.project_id = ".$this->getCurrentProjectId()."
+				and _b.project_id = ".$project_id."
 			left join %PRE%pages_taxa _c 
 				on _a.page_id = _c.id 
-				and _c.project_id = ".$this->getCurrentProjectId()."
-			where _a.taxon_id = ".$data['id']."
-			and _a.project_id = ".$this->getCurrentProjectId()
-		);
-		$res['content']=$t;
+				and _c.project_id = ".$project_id."
+			where _a.taxon_id = ".$taxon_id."
+			and _a.project_id = ".$project_id
+		;
 
+		$res['content']=$this->freeQuery( $query );
 
-		$t=$this->models->Taxon->freeQuery("
+		$query="
 			select _a.value as file_name,_b.value as copyright, '1' as overview_image 
 			from %PRE%nbc_extras _a
 			left join %PRE%nbc_extras _b 
 				on _b.ref_type = 'taxon' 
 				and _b.ref_id=_a.ref_id 
 				and _b.name='photographer' 
-				and _b.project_id = ".$this->getCurrentProjectId()."
-			where _a.ref_id = ".$data['id']." 
+				and _b.project_id = ".$project_id."
+			where _a.ref_id = ".$taxon_id." 
 				and _a.ref_type='taxon' 
 				and _a.name='url_image'
-			and _a.project_id = ".$this->getCurrentProjectId()
-		);
-		$res['img_main']=$t;
+			and _a.project_id = ".$project_id
+		;
+
+		$res['img_main']=$this->freeQuery( $query );
 
 
-		$t=$this->models->Taxon->freeQuery("
-			select file_name from %PRE%media_taxon where taxon_id = ".$data['id']." and project_id = ".$this->getCurrentProjectId()
-		);
-		$res['img_other']=$t;
+		$query="select file_name from %PRE%media_taxon where taxon_id = ".$taxon_id." and project_id = ".$project_id;
+
+		$res['img_other']=$this->freeQuery( $query );
 
 
-		$t=$this->models->Taxon->freeQuery("
+		$query="
 			select 'taxon' as type, _b.id as id, _b.taxon as taxon,_c.commonname as label, _n.value as img 
 			from %PRE%taxa_relations _a 
 			left join %PRE%taxa _b 
 				on _b.id = _a.relation_id 
-				and _b.project_id = ".$this->getCurrentProjectId()."
+				and _b.project_id = ".$project_id."
 			left join %PRE%commonnames _c 
 				on _c.taxon_id = _b.id 
-				and _c.project_id = ".$this->getCurrentProjectId()."
+				and _c.project_id = ".$project_id."
 			left join %PRE%nbc_extras _n 
 				on _b.id = _n.ref_id 
 				and _n.ref_type='taxon' 
 				and _n.name='url_thumbnail' 
-				and _n.project_id = ".$this->getCurrentProjectId()."
-			where _a.ref_type='taxon' and _a.taxon_id = ".$data['id']." 
-			and _a.project_id = ".$this->getCurrentProjectId()."
+				and _n.project_id = ".$project_id."
+			where _a.ref_type='taxon' and _a.taxon_id = ".$taxon_id." 
+			and _a.project_id = ".$project_id."
 			union all
 			select 'variation' as type, _e.id as id,  _f.taxon as taxon, _e.label as label, _n.value as img 
 			from %PRE%taxa_relations _d 
 			left join %PRE%taxa_variations _e 
 				on _e.id = _d.relation_id 
-				and _e.project_id = ".$this->getCurrentProjectId()."
+				and _e.project_id = ".$project_id."
 			left join %PRE%taxa _f 
 				on _f.id = _d.taxon_id 
-				and _f.project_id = ".$this->getCurrentProjectId()."
+				and _f.project_id = ".$project_id."
 			left join %PRE%nbc_extras _n 
 				on _d.taxon_id = _n.ref_id 
 				and _n.ref_type='taxon' 
 				and _n.name='url_thumbnail' 
-				and _n.project_id = ".$this->getCurrentProjectId()."
+				and _n.project_id = ".$project_id."
 			where _d.ref_type='variation'  
-			and _d.taxon_id =".$data['id']." 
-			and _d.project_id = ".$this->getCurrentProjectId()
-		);
-		$res['similar']=$t;
-
-		$t=$this->models->Taxon->_get(array('id'=>array('project_id' => $this->getCurrentProjectId(),'parent_id'=>$data['id'])));
-		foreach((array)$t as $key => $val)
-		{
-			$t[$key]['label']=$this->getCommonname($val['id']);
-			$t[$key]['img']=$this->getNbcExtras(array('id'=>$val['id'],'name'=>'url_thumbnail'));
-		}
-		if ($t)
-		{
-			usort($t,function($a,$b) {return ($a['label']>$b['label']?1:-1);});
-			$res['children']=$t;
-		}
+			and _d.taxon_id =".$taxon_id." 
+			and _d.project_id = ".$project_id
+		;
+		$res['similar']=$this->freeQuery( $query );
 
 		return $res;
 					
 	}
-	
 
     public function getGuiMenuOrder( $params )
     {
@@ -149,7 +395,7 @@ taxon_id	$data['id']
 		$language_id = isset($params['language_id']) ? $params['language_id'] : null;
 		$matrix_id = isset($params['matrix_id']) ? $params['matrix_id'] : null;
 		
-		if ( is_null($project_id)  ||  is_null($language_id)   ||  is_null($matrix_id) )
+		if ( is_null($project_id) || is_null($language_id) || is_null($matrix_id) )
 			return;
 		
 		$query="
