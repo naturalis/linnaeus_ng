c85d3ea73612f57b552abb792204ac9345e89809
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2016-02-09 16:08:24 +0100

    media eerste babystapjes

--- configuration/admin/controllers/MediaController.php
+++ configuration/admin/controllers/MediaController.php
@@ -1,28 +1,29 @@
 <?php
 
+/*
+ * media.js should be loaded in "parent" controller!
+ */
+
 include_once ('Controller.php');
 class MediaController extends Controller
 {
 
-    public $usedModels = array(
-    );
+    private $_rsBaseUrl;
+    private $_rsMasterKey;
+    private $_rsUserKey;
+    private $_rsCollectionId;
+    private $_rsSearchApi;
+    private $_rsUploadApi;
+
+    private $_rsSearchUrl;
+    private $_rsUploadUrl;
+    private $_rsNewUserUrl;
+
+    private $_curlResult;
+
+    public $usedModels = array();
 
     public $controllerPublicName = 'Media';
-
-	public $cssToLoad = array('prettyPhoto/prettyPhoto.css');
-
-    public $jsToLoad = array(
-        'all' => array(
-            'main.js',
-            'prettyPhoto/jquery.prettyPhoto.js'
-        ),
-        'IE' => array()
-    );
-
-    public $usedHelpers = array(
-        //'file_upload_helper',
-        //'session_module_settings'
-    );
 
 
     /**
@@ -33,9 +34,12 @@ class MediaController extends Controller
     public function __construct ()
     {
         parent::__construct();
+        $this->setConfigSettings();
+        $this->setRsApiUrls();
+
+        //$this->createUser();
+       // die(print_r($this->_curlResult));
     }
-
-
 
     /**
      * Destroys!
@@ -47,309 +51,125 @@ class MediaController extends Controller
         parent::__destruct();
     }
 
-
-
-    /**
-     * View displaying 'not authorized'
-     *
-     * Users can be redirected to notAuthorizedAction from every controller,
-     * so the controller name is hidden in the output to avoid confusion.
-     *
-     * @access    public
-     */
-    public function notAuthorizedAction ()
+    private function setRsBaseUrl ()
     {
-        $this->smarty->assign('hideControllerPublicName', true);
-
-        $this->addError($this->translate('You are not authorized to do that.'));
-
-		if (isset($_SESSION['admin']['project']['lead_experts']))
-		{
-			if (count((array)$_SESSION['admin']['project']['lead_experts'])==1)
-			{
-				$this->addMessage($this->translate('To gain access to the page you were attempting to view, please contact the lead expert of your project:'));
-			}
-			else
-			{
-				$this->addMessage($this->translate('To gain access to the page you were attempting to view, please contact one of the lead experts of your project:'));
-			}
-
-			foreach((array)$_SESSION['admin']['project']['lead_experts'] as $key => $val)
-			{
-				$this->addMessage($val['first_name'].' '.$val['last_name'].' (<a href="mailto:'.$val['email_address'].'">'.$val['email_address'].'</a>)');
-			}
-
-		}
-
-        $this->printPage();
+        // To do: get from config
+        $this->_rsBaseUrl = 'http://localhost/resourcespace/plugins/';
     }
 
-    /**
-	* Project wide index, showing start screen with module icons
-	*
-	* @access     public
-	*/
-    public function adminIndexAction ()
+    private function setRsMasterKey ()
     {
-        $this->checkAuthorisation();
+        // To do: get from config
+        $this->_rsMasterKey = 'LDpWw7lzzeM5Vbck0K5N2JBjn2nQiobY6Qxr195NPzqXxXNfeDIjqd96JuomeuIo1Gi8TsM7JjR8d7WgCvX-a8P3mz7WWHgfnZIyJ5KF1UAKAkV3VCjk0zt3Qyo3I4jM';
+    }
 
-        $this->includeLocalMenu = true;
+    private function setRsUserKey ()
+    {
+        // To do: get from config
+        $this->_rsUserKey = 'mgnwUMXYDrxXQIWaA5V-ZzioDNExJhVwHQ-8scfwAawkfFAf8su4znTstVSIkMATeNA0pMhPQYok0C_if_k-sKNOL7uZ9o_VCesg6_Wi5j8Dbcs7yUud2tu05N4gGKMk';
+    }
 
-        $this->setPageName($this->translate('Project overview'));
+    private function setRsCollectionId ()
+    {
+        // To do: get from config
+        $this->_rsCollectionId = 5;
+    }
 
-        // get all modules activated in this project
-        $modules = $this->models->ModulesProjects->_get(array(
-            'id' => array(
-                'project_id' => $this->getCurrentProjectId()
-            ),
-            'order' => 'module_id asc'
-        ));
+    private function setRsSearchApi ()
+    {
+        // To do: get from config
+        $this->_rsSearchApi = 'api_search_lng';
+    }
 
-        foreach ((array) $modules as $key => $val)
-		{
-            // get info per module
-            $mp = $this->models->Modules->_get(array(
-                'id' => $val['module_id']
-            ));
+    private function setRsNewUserApi ()
+    {
+        // To do: get from config
+        $this->_rsNewUserApi =  'api_new_user_lng';
+    }
 
-            $modules[$key]['icon'] = $mp['icon'];
-            $modules[$key]['module'] = $mp['module'];
-            $modules[$key]['controller'] = $mp['controller'];
-            $modules[$key]['show_in_menu'] = $mp['show_in_menu'];
+    private function setRsUploadApi ()
+    {
+        // To do: get from config
+        $this->_rsUploadApi =  'api_upload_lng';
+    }
 
-            // see if the current user has any rights within the module
-            if (isset($_SESSION['admin']['user']['_rights'][$this->getCurrentProjectId()][$mp['controller']]) || $this->isCurrentUserSysAdmin())
-                $modules[$key]['_rights'] = @$_SESSION['admin']['user']['_rights'][$this->getCurrentProjectId()][$mp['controller']];
-        }
+    private function setConfigSettings ()
+    {
+        $this->setRsBaseUrl();
+        $this->setRsMasterKey();
+        $this->setRsUserKey();
+        $this->setRsCollectionId();
+        $this->setRsSearchApi();
+        $this->setRsNewUserApi();
+        $this->setRsUploadApi();
+    }
 
-        $freeModules = $this->models->FreeModulesProjects->_get(array(
-            'id' => array(
-                'project_id' => $this->getCurrentProjectId()
-            )
-        ));
+    private function setRsApiUrls ()
+    {
+        // Search url: &search=[term]* to be appended
+        $this->_rsSearchUrl = $this->_rsBaseUrl . $this->_rsSearchApi . '/?' .
+            'key=' . $this->_rsUserKey . '&prettyfieldnames=true&collection=' .
+            $this->_rsCollectionId;
 
-        foreach ((array) $freeModules as $key => $val)
-		{
-            // see if the current user has any rights within the module
-            if (
-				(
-					isset($_SESSION['admin']['user']['_rights'][$this->getCurrentProjectId()]['_freeModules'][$val['id']]) &&
-					$_SESSION['admin']['user']['_rights'][$this->getCurrentProjectId()]['_freeModules'][$val['id']] === true
-				) ||  $this->isCurrentUserSysAdmin()
-			) $freeModules[$key]['currentUserRights'] = true;
-        }
+        // Upload url: &field8=[title] to be appended
+        $this->_rsUploadUrl = $this->_rsBaseUrl . $this->_rsUploadApi . '/?' .
+            'key=' . $this->_rsUserKey . '&collection=' . $this->_rsCollectionId;
 
-        unset($_SESSION['admin']['user']['freeModules']['activeModule']);
-
-        $this->smarty->assign('modules', $modules);
-        $this->smarty->assign('freeModules', $freeModules);
-        $this->smarty->assign('currentUserRoleId', $this->getCurrentUserRoleId());
-
-        $this->printPage();
+        // New user url; newuser appended in createUser()
+        $this->_rsNewUserUrl = $this->_rsBaseUrl . $this->_rsNewUserApi . '/?' .
+            'key=' . $this->_rsMasterKey;
     }
 
 
 
-    /**
-     * View displaying warning that the accessed module is not part of the current project
-     *
-     * Users can be redirected to moduleNotPresentAction from every controller,
-     * so the controller name is hidden in the output to avoid confusion.
-     *
-     * @access    public
-     */
-    public function moduleNotPresentAction ()
-    {
-        $this->smarty->assign('hideControllerPublicName', true);
-
-		if (isset($_SESSION['admin']['system']['last_module_name']))
-	        $this->addError(sprintf($this->translate('The module "%s" is not part of your project.'),$_SESSION['admin']['system']['last_module_name']));
-		else
-	        $this->addError(sprintf($this->translate('The module you tried to start is not part of your project.')));
-
-        $this->printPage();
-
-    }
-
-
-	public function rootIndexAction()
-	{
-		$this->isMultiLingual = false;
-		$this->includeLocalMenu = false;
-		$this->printBreadcrumbs = false;
-
-		$projects = $this->models->Project->_get(array('id' => array('published' => 1)));
-
-		$this->smarty->assign('projects',$projects);
-		$this->smarty->assign('excludeLogout',true);
-		$this->smarty->assign('breadcrumbs',false);
-
-		$this->printPage('utilities/root_index');
-	}
-
-	public function massUploadAction()
-	{
-        $this->checkAuthorisation();
-
-		$this->setPageName('Mass upload');
-
-		if ($this->requestDataFiles && !$this->isFormResubmit())
-		{
-			$this->loadControllerConfig('Species');
-			$filesToSave = $this->getUploadedMediaFiles(array('overwrite'=>$this->rGetVal('overwrite')));
-			$this->loadControllerConfig();
-
-			if ($filesToSave)
-			{
-				foreach ((array) $filesToSave as $key => $file)
-				{
-					$this->addMessage(sprintf('Uploaded %s','<code>'.$file['name'].'</code>'));
-                }
-			}
-		}
-
-		$this->loadControllerConfig('Species');
-
-		$this->smarty->assign('allowedFormats', $this->controllerSettings['media']['allowedFormats']);
-
-		$this->smarty->assign('iniSettings', array(
-			'upload_max_filesize' => ini_get('upload_max_filesize'),
-			'post_max_size' => ini_get('post_max_size'),
-			'maximum' => min(intval(ini_get('post_max_size')),intval(ini_get('upload_max_filesize')))
-		));
-
-        $this->printPage();
-	}
-
-	private function deleteFile($id)
-	{
-		$d=$this->moduleSession->getModuleSetting( 'mediaFiles' );
-
-		if (isset($d['files'][$id]))
-		{
-			return unlink($this->getProjectsMediaStorageDir().$d['files'][$id]);
-		}
-		else
-		{
-			$this->addError('Unknown file index.');
-			return false;
-		}
-	}
-
-	public function browseMediaAction()
-	{
-
-        $this->checkAuthorisation();
-
-		$this->setPageName('Browse media');
-
-		if ($this->rHasVal('action','delete') && $this->rHasId())
-		{
-			if ($this->deleteFile($this->rGetId()))
-			{
-				$this->redirect('browse_media.php#');
-			}
-		}
-		else
-		if ($this->rHasVal('action','delete') && $this->rHasVal('delete') && !$this->isFormResubmit())
-		{
-			foreach((array)$this->rGetVal('delete') as $val)
-			{
-				$this->deleteFile($val);
-			}
-
-		}
-		else
-		if ($this->rHasVal('action','purge') && !$this->isFormResubmit())
-		{
-			foreach(glob($this->getProjectsMediaStorageDir().'/*') as $file)
-			{
-				if (filetype($file)=='file')
-				{
-					unlink($file);
-				}
-			}
-		}
-
-		foreach(glob($this->getProjectsMediaStorageDir().'/*') as $file)
-		{
-			if (filetype($file)=='dir')
-				$r['dirs'][] = basename($file);
-			else
-			if (filetype($file)=='file')
-				$r['files'][] = basename($file);
-		}
-
-		$this->moduleSession->setModuleSetting( array('setting'=>'mediaFiles','value'=>$r ) );
-
-		$this->smarty->assign('files',$r);
-
-		$this->printPage();
-
-	}
-
-	private function renameMedia($p)
-	{
-
-		$id = isset($p['id']) ? $p['id'] : null;
-		$name = isset($p['name']) ? $p['name'] : null;
-
-		if (!isset($id) || !isset($name))
-			return false;
-
-		$d=$this->moduleSession->getModuleSetting( 'mediaFiles' );
-
-		if (isset($d['files'][$id]))
-		{
-			if (file_exists($this->getProjectsMediaStorageDir().$name))
-			{
-				return false;
-			}
-			else
-			{
-				return rename(
-					$this->getProjectsMediaStorageDir().$d['files'][$id],
-					$this->getProjectsMediaStorageDir().$name
-				);
-			}
-		}
-		else
-		{
-			return false;
-		}
-
-	}
-
-    /**
-     * AJAX interface for this class
-     *
-     * @access    public
-     */
     public function ajaxInterfaceAction ()
     {
-        if (null==$this->rHasVal('action')) return;
 
-		if ($this->rGetVal('action')=='translate')
-		{
-			$this->smarty->assign('returnText',$this->javascriptTranslate($this->rGetVal('text')));
-		}
-		else
-		if ($this->rGetVal('action')=='change_media_name')
-		{
-			$this->smarty->assign('returnText',$this->renameMedia($this->rGetAll()));
-		}
-		else
-		if ($this->rGetVal('action')=='set_something')
-		{
-			$this->moduleSession->setModuleSetting( array('setting'=>$this->rGetVal('name'),'value'=>$this->rGetVal('value') ) );
-		}
-		else
-		if ($this->rGetVal('action')=='get_something')
-		{
-			$this->smarty->assign('returnText',json_encode($this->moduleSession->getModuleSetting( $this->rGetVal('name') ) ) );
-		}
-
-		$this->printPage();
     }
+
+    public function getMediaFiles ($p)
+    {
+        $search = isset($p['search']) ? $p['search'] : false; // empty to return everything
+        $sort = isset($p['sort']) ? $p['sort'] : false; // asc (default)/desc
+
+        $url = $this->_rsSearchUrl .
+            ($search ? '&search=' . urlencode($search) . '*' : '') .
+            ($sort ? '&sort=' . urlencode($sort) : '');
+        $this->_curlResult = $this->getCurlResult($url);
+    }
+
+
+    public function uploadMedia ($p)
+    {
+        $file = is_array($p) && isset($p['file']) ? $p['file'] : $p;
+        if (empty($file) || !file_exists(realpath($file))) {
+            // Todo: die
+            return 'error';
+        }
+        $post['userfile'] = new CURLFile($file);
+        if (isset($p['title']) && empty($p['title'])) {
+            $post['field8'] = $p['title'];
+        }
+        $this->_curlResult = $this->getCurlResult(array(
+            'url' => $this->_rsUploadUrl,
+            'post' => $post
+        ));
+        // Part of data should be stored in Linnaeus!!
+
+        // Check for error
+    }
+
+    public function createUser ()
+    {
+        $project = $this->getCurrentProjectData();
+        $newUser = $project['title'] . ' @ ' . $_SERVER['SERVER_NAME'];
+        $this->_curlResult =
+            $this->getCurlResult($this->_rsNewUserUrl . '&newuser=' . urlencode($newUser));
+
+        // Data should be stored in Linnaeus!!
+
+        // Check for error
+    }
+
 
 }
