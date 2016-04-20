5723299cd721619d7a9c89090a5f245d66d5cdaf
maarten schermer <maarten.schermer@naturalis.nl>
2016-04-20 11:16:06 +0200

    fixes

---
+++ database/drop_obsolete_tables.sql
@@ -0,0 +1,25 @@
+drop table beelduitwisselaar_batches;
+drop table choices_content_keysteps_undo;
+drop table content_keysteps_undo;
+drop table content_taxa_undo;
+drop table diversity_index;
+drop table diversity_index_old;
+drop table dna_barcodes;
+drop table dump;
+drop table geodata_types;
+drop table geodata_types_titles;
+drop table heartbeats;
+drop table helptexts;
+drop table hybrids;
+drop table l2_diversity_index;
+drop table l2_maps;
+drop table l2_occurrences_taxa;
+drop table l2_occurrences_taxa_combi;
+drop table literature;
+drop table names_temp;
+/* drop table nbc_extras; */
+drop table occurrences_taxa;
+drop table settings;
+drop table taxon_trend_years;
+drop table taxon_trends;
+drop table users_taxa;
