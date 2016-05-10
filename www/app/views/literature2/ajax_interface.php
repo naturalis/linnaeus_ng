42563ddb43d6059cc86a2e4b450f506426ef663f
maarten schermer <maarten.schermer@naturalis.nl>
2016-05-10 11:13:49 +0200

    lit2 front end

---
+++ www/app/views/literature2/ajax_interface.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once ('../../../../configuration/app/controllers/Literature2Controller.php');
+
+$c = new Literature2Controller();
+
+$c->setStoreHistory(false);
+
+$c->ajaxInterfaceAction();
