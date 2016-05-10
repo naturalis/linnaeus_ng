7c3f62a7ccb17ca4d1ad5a5b5b2ad6fd1cba42cc
maarten schermer <maarten.schermer@naturalis.nl>
2016-05-10 13:31:05 +0200

    more!

--- configuration/app/models/LinnaeusModel.php
+++ configuration/app/models/LinnaeusModel.php
@@ -26,36 +26,6 @@ final class LinnaeusModel extends AbstractModel
         parent::__destruct();
     }
 
-	public function getIntroductionTopicId( $params )
-    {
-
-		$project_id = isset($params['project_id']) ? $params['project_id'] : null;
-		$topic = isset($params['topic']) ? $params['topic'] : null;
-
-		if ( is_null($project_id) || is_null($topic) ) return;
-
-		$query = "
-			select
-
-				*
-
-			from %PRE%introduction_pages _a
-
-			right join %PRE%content_introduction _c
-				on _a.id = _c.page_id
-				and _a.project_id=_c.project_id
-				and _c.topic = '" .  mysqli_real_escape_string($this->databaseConnection, $topic) . "' 
-				
-			where
-				_a.project_id = ".$project_id . "
-			limit 1
-		";	
-		
-		$d=$this->freeQuery( $query );
-		
-		return $d ? $d[0]['id'] : null;
-	}
-
 	public function getFirstIntroductionTopicId( $params )
     {
 
