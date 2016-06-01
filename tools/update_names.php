b5b63a98c229ceea782374b1345e5a7fd17682f9
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2016-06-01 13:17:33 +0200

    update names script verbeterd (sommige projecten bleken niet mee te komen)

--- tools/update_names.php
+++ tools/update_names.php
@@ -36,27 +36,31 @@
 	$r = mysqli_query($d, $q) or die($q . mysqli_error($d));
 
     echo "Updating name types...\n";
-    $q = 'SELECT `id` FROM `projects`';
+    $q = 'SELECT `id`, `title` FROM `projects` ORDER BY `title`';
     $r = mysqli_query($d, $q);
     while ($row = mysqli_fetch_assoc($r)) {
-        $projectIds[] = $row['id'];
+        $projects[$row['id']] = $row['title'];
     }
-    setNameTypes($projectIds);
 
-    echo "Clearing previously inserted names...\n";
-    clearNames($projectIds);
+    foreach ($projects as $projectId => $title) {
+        echo "Processing $title\n";
 
-    echo "Inserting taxa...\n";
-    insertTaxa($projectIds);
+        echo "Updating name types...\n";
+        setNameTypes($projectId);
 
-    echo "Updating (infra)species names...\n";
-    updateTaxa();
+        echo "Inserting taxa...\n";
+        insertTaxa($projectId);
 
-    echo "Inserting synonyms...\n";
-    insertSynonyms($projectIds);
+        echo "Updating (infra)species names...\n";
+        updateTaxa($projectId);
 
-    echo "Inserting common names...\n";
-    insertCommonNames();
+        echo "Inserting synonyms...\n";
+        insertSynonyms($projectId);
+
+        echo "Inserting common names...\n\n";
+        insertCommonNames($projectId);
+
+    }
 
 	$q = 'ALTER TABLE `names` DROP INDEX `delete_me`';
 	$r = mysqli_query($d, $q) or die($q . mysqli_error($d));
@@ -79,26 +83,24 @@
         }
 	}
 
-	function setNameTypes ($projectIds) {
+	function setNameTypes ($projectId) {
 	    global $d, $nameTypes;
-        foreach ($projectIds as $id) {
-            foreach ($nameTypes as $type) {
-                $q = "INSERT IGNORE INTO `name_types` VALUES (NULL,$id,'$type',NOW(),NOW());";
-                mysqli_query($d, $q) or die($q . mysqli_error($d));
-            }
+        foreach ($nameTypes as $type) {
+            $q = "INSERT IGNORE INTO `name_types` VALUES (NULL,$projectId,'$type',NOW(),NOW());";
+            mysqli_query($d, $q) or die($q . mysqli_error($d));
         }
 	}
 
-	function clearNames ($projectIds) {
+	function clearNames () {
 	    global $d;
 	    mysqli_query($d, 'truncate table names') or die($q . mysqli_error($d));
 	}
 
-	function insertTaxa ($projectIds) {
+	function insertTaxa ($projectId) {
 	    global $d, $scientificId;
 	    $q = "insert into names (project_id,taxon_id,language_id,type_id,name,authorship,created)
         	select
-        		[id],
+        		$projectId,
         		_a.id,
             	$scientificId,
         		_b.id,
@@ -113,22 +115,21 @@
         		and _b.nametype = 'isValidNameOf'
 
         	where
-        		_a.project_id=[id]
+        		_a.project_id=$projectId
         		and _a.taxon is not null";
 
-	    foreach ($projectIds as $id) {
-	        $q = str_replace('[id]', $id, $q);
-	        mysqli_query($d, $q) or die($q . mysqli_error($d));
-        }
+        mysqli_query($d, $q) or die($q . mysqli_error($d));
 	}
 
-	function updateTaxa () {
+	function updateTaxa ($projectId) {
 	    global $d;
 	    $abbreviations = array();
 	    // First get genera and lower for all projects; species = 74
-	    $q = 'select t1.id, t2.abbreviation from projects_ranks as t1
-            left join ranks as t2 on t2.id = t1.rank_id
-            where t1.rank_id >= 63';
+	    $q = 'select t1.id, t2.abbreviation from
+	          projects_ranks as t1
+              left join ranks as t2 on t2.id = t1.rank_id
+              where t1.rank_id >= (select id from ranks where rank = "genus")
+	          and project_id = ' . $projectId;
 	    mysqli_query($d, $q) or die($q . mysqli_error($d));
 	    $r = mysqli_query($d, $q) or die($q . mysqli_error($d));
 	    while ($row = mysqli_fetch_assoc($r)) {
@@ -137,30 +138,37 @@
                 $abbreviations[] = $row['abbreviation'];
             }
 	    }
-	    $q = 'select id, taxon from taxa where rank_id in (' . implode(',', $rankIds) . ') order by project_id';
-	    $r = mysqli_query($d, $q) or die($q . mysqli_error($d));
-		while ($row = mysqli_fetch_assoc($r)) {
-            $parts = explode(' ', $row['taxon']);
-            $q2 = 'update `names` set uninomial = "' . mysqli_real_escape_string($d, $parts[0]) . '"';
-            if (isset($parts[1])) {
-                $q2 .=  ', specific_epithet = "' . mysqli_real_escape_string($d, $parts[1]) . '"';
-            }
-            $infra = 'null';
-		    if (isset($parts[2]) && !in_array($parts[2], $abbreviations)) {
-		        $infra = '"' . mysqli_real_escape_string($d, $parts[2]) . '"';
-            } else if (isset($parts[2]) && in_array($parts[2], $abbreviations) && isset($parts[3])) {
-                $infra = '"' . mysqli_real_escape_string($d, $parts[3]) . '"';
-            }
-            $q2 .=  ', infra_specific_epithet = ' . $infra . ' where taxon_id = '. $row['id'] . ' and type_id = 1';
-		    mysqli_query($d, $q2) or die($q2 . mysqli_error($d));
-		}
+
+	    if (isset($rankIds) && !empty($rankIds)) {
+    	    $q = 'select id, taxon from taxa where rank_id in (' . implode(',', $rankIds) . ') and
+    	          project_id = ' . $projectId;
+    	    $r = mysqli_query($d, $q) or die($q . mysqli_error($d));
+
+    		while ($row = mysqli_fetch_assoc($r)) {
+                $parts = explode(' ', $row['taxon']);
+                $q2 = 'update `names` set uninomial = "' . mysqli_real_escape_string($d, $parts[0]) . '"';
+                if (isset($parts[1])) {
+                    $q2 .=  ', specific_epithet = "' . mysqli_real_escape_string($d, $parts[1]) . '"';
+                }
+                $infra = 'null';
+    		    if (isset($parts[2]) && !in_array($parts[2], $abbreviations)) {
+    		        $infra = '"' . mysqli_real_escape_string($d, $parts[2]) . '"';
+                } else if (isset($parts[2]) && in_array($parts[2], $abbreviations) && isset($parts[3])) {
+                    $infra = '"' . mysqli_real_escape_string($d, $parts[3]) . '"';
+                }
+                $q2 .=  ', infra_specific_epithet = ' . $infra .
+                ' where taxon_id = '. $row['id'] . ' and type_id = 1 and project_id = ' . $projectId;
+
+    		    mysqli_query($d, $q2) or die($q2 . mysqli_error($d));
+    		}
+	    }
 	}
 
-	function insertSynonyms ($projectIds) {
+	function insertSynonyms ($projectId) {
 	    global $d, $scientificId;
 	    $q = "insert into names (project_id,taxon_id,language_id,type_id,name,authorship,created)
         	select
-        		[id],
+        		$projectId,
         		_a.taxon_id,
         		$scientificId,
         		_b.id,
@@ -175,19 +183,19 @@
         		and _b.nametype = 'isSynonymOf'
 
         	where
-        		_a.project_id=[id]
+        		_a.project_id=$projectId
         		and _a.synonym is not null";
 
-	    foreach ($projectIds as $id) {
-	        $q = str_replace('[id]', $id, $q);
-            mysqli_query($d, $q) or die($q . mysqli_error($d));
-        }
+        mysqli_query($d, $q) or die($q . mysqli_error($d));
 	}
 
-	function insertCommonNames () {
+	function insertCommonNames ($projectId) {
 	    global $d;
-	    $q = "SELECT * FROM `commonnames` ORDER BY project_id, taxon_id, language_id, show_order";
+	    $q = "SELECT * FROM `commonnames`
+	          WHERE project_id = $projectId
+	          ORDER BY taxon_id, language_id, show_order";
 	    $r = mysqli_query($d, $q) or die($q . mysqli_error($d));
+
 	    $oldTest = false;
 	    while ($row = mysqli_fetch_assoc($r)) {
             $test = $row['taxon_id'] . $row['language_id'];
@@ -197,7 +205,6 @@
             $q2 = 'insert into names (project_id,taxon_id,language_id,type_id,name,created) values (' .
                 $row['project_id'] . ', ' . $row['taxon_id'] . ', ' . $row['language_id'] . ', ' .
                 $typeId . ', "' .  mysqli_real_escape_string($d, $row['commonname']) . '", NOW());';
-//echo "$q2<br>";
             mysqli_query($d, $q2) or die($q2 . mysqli_error($d));
             $oldTest = $test;
 	    }
