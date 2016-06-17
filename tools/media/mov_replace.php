4e885927cfd55f6fb0005bb6dfb4c3826f074843
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2016-06-17 17:08:20 +0200

    turtles

--- tools/media/mov_replace.php
+++ tools/media/mov_replace.php
@@ -1,21 +1,13 @@
 <?php
 
-    $projectId = 135;
+    $projectId = 11;
 
 
 $replacement = array(
 
-'alarm call juveniles.mov' => 'alarm call juveniles.mp3',
-'call american bullfrog.mov' => 'call american bullfrog.mp3',
-'call common frog.mov' => 'call common frog.mp3',
-'call common toad.mov' => 'call common toad.mp3',
-'call edible frog.mov' => 'call edible frog.mp3',
-'call european tree frog.mov' => 'call european tree frog.mp3',
-'call marsh frog.mov' => 'call marsh frog.mp3',
-'call midwife toad.mov' => 'call midwife toad.mp3',
-'call natterjack toad.mov' => 'call natterjack toad.mp3',
-'call pool frog.mov' => 'call pool frog.mp3',
-'call yellow-bellied toad.mov' => 'call yellow-bellied toad.mp3',
+'chelonia mydas agassizii.mov' => 'chelonia mydas agassizii.mp4',
+'eretmochelys i bissa.mov' => 'eretmochelys i bissa.mp4',
+'geochelone radiata.mov' => 'geochelone radiata.mp4',
 
 );
 
