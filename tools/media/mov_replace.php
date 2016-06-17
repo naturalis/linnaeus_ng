46fc446fa8989ea27b8db2f29377f869e6ffec30
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2016-06-17 17:13:03 +0200

    insecten

--- tools/media/mov_replace.php
+++ tools/media/mov_replace.php
@@ -1,13 +1,54 @@
 <?php
 
-    $projectId = 11;
+    $projectId = 85;
 
 
 $replacement = array(
 
-'chelonia mydas agassizii.mov' => 'chelonia mydas agassizii.mp4',
-'eretmochelys i bissa.mov' => 'eretmochelys i bissa.mp4',
-'geochelone radiata.mov' => 'geochelone radiata.mp4',
+'acheta_d.mov' => 'acheta_d.mp3',
+'acrypter.mov' => 'acrypter.mp3',
+'aeropus.mov' => 'aeropus.mp3',
+'barbitis.mov' => 'barbitis.mp3',
+'bryodema.mov' => 'bryodema.mp3',
+'callipta.mov' => 'callipta.mp3',
+'chorthip.mov' => 'chorthip.mp3',
+'chorthp2.mov' => 'chorthp2.mp3',
+'chorthp3.mov' => 'chorthp3.mp3',
+'chorthp4.mov' => 'chorthp4.mp3',
+'chorthp5.mov' => 'chorthp5.mp3',
+'chorthp6.mov' => 'chorthp6.mp3',
+'chrysoch.mov' => 'chrysoch.mp3',
+'conoceph.mov' => 'conoceph.mp3',
+'decticus.mov' => 'decticus.mp3',
+'ephippig.mov' => 'ephippig.mp3',
+'euthysti.mov' => 'euthysti.mp3',
+'gampsocl.mov' => 'gampsocl.mp3',
+'gomphoce.mov' => 'gomphoce.mp3',
+'gryllota.mov' => 'gryllota.mp3',
+'gryllus.mov' => 'gryllus.mp3',
+'isophya.mov' => 'isophya.mp3',
+'leptophy.mov' => 'leptophy.mp3',
+'locusta.mov' => 'locusta.mp3',
+'meconema.mov' => 'meconema.mp3',
+'metriopt.mov' => 'metriopt.mp3',
+'metriot2.mov' => 'metriot2.mp3',
+'myrmeleo.mov' => 'myrmeleo.mp3',
+'nemobius.mov' => 'nemobius.mp3',
+'oecanthu.mov' => 'oecanthu.mp3',
+'oedipoda.mov' => 'oedipoda.mp3',
+'omocestu.mov' => 'omocestu.mp3',
+'omocesu2.mov' => 'omocesu2.mp3',
+'phanerop.mov' => 'phanerop.mp3',
+'pholidop.mov' => 'pholidop.mp3',
+'pholidp2.mov' => 'pholidp2.mp3',
+'platycle.mov' => 'platycle.mp3',
+'polysarc.mov' => 'polysarc.mp3',
+'psophus.mov' => 'psophus.mp3',
+'sphingon.mov' => 'sphingon.mp3',
+'stenobot.mov' => 'stenobot.mp3',
+'stethoph.mov' => 'stethoph.mp3',
+'tettign2.mov' => 'tettign2.mp3',
+'tettigon.mov' => 'tettigon.mp3',
 
 );
 
