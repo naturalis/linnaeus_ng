f16f6193686e4a2c4cb52ca37ad9cf296b593f6f
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2016-06-17 16:59:37 +0200

    rept nl

--- tools/media/mov_replace.php
+++ tools/media/mov_replace.php
@@ -1,15 +1,23 @@
 <?php
 
-    $projectId = 7;
+    $projectId = 67;
 
 
 $replacement = array(
 
-'c_mydas.mov' => 'c_mydas.mp4',
-'d_americ.mov' => 'd_americ.mp4',
-'dive.mov' => 'dive.mp4',
-'l_polygo.mov' => 'l_polygo.mp4',
-'p_cruent.mov' => 'p_cruent.mp4',
+'alarmroep juvenielen.mov' => 'alarmroep juvenielen.mp3',
+'paarroep bastaardkikker.mov' => 'paarroep bastaardkikker.mp3',
+'paarroep boomkikker.mov' => 'paarroep boomkikker.mp3',
+'paarroep bruine kikker.mov' => 'paarroep bruine kikker.mp3',
+'paarroep geelbuikvuurpad.mov' => 'paarroep geelbuikvuurpad.mp3',
+'paarroep gewone pad.mov' => 'paarroep gewone pad.mp3',
+'paarroep heikikker.mov' => 'paarroep heikikker.mp3',
+'paarroep meerkikker.mov' => 'paarroep meerkikker.mp3',
+'paarroep poelkikker.mov' => 'paarroep poelkikker.mp3',
+'paarroep rugstreeppad.mov' => 'paarroep rugstreeppad.mp3',
+'paarroep stierkikker.mov' => 'paarroep stierkikker.mp3',
+'paarroep vroedmeesterpad.mov' => 'paarroep vroedmeesterpad.mp3',
+'paarroep_boomkikker.mov' => 'paarroep_boomkikker.mp3',
 
 );
 
