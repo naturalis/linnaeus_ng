137ffcbb638f70442ca21ed0906f86d6388a27a5
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2016-06-17 15:21:52 +0200

    euph mov conversie

--- tools/media/mov_replace.php
+++ tools/media/mov_replace.php
@@ -1,44 +1,36 @@
 <?php
 
-    $projectId = 59;
+    $projectId = 8;
 
 
 $replacement = array(
 
-'b_musculus_track_1.mov' => 'b_musculus_track_1.mp3',
-'common__striped_d_track_1.mov' => 'common__striped_d_track_1.mp3',
-'common__striped_d_track_2.mov' => 'common__striped_d_track_2.mp3',
-'d_delphis_track_1.mov' => 'd_delphis_track_1.mp3',
-'d_delphis_track_2.mov' => 'd_delphis_track_2.mp3',
-'d_delphis_track_3.mov' => 'd_delphis_track_3.mp3',
-'l_obscurus_track_1.mov' => 'l_obscurus_track_1.mp3',
-'o_orca_track_1.mov' => 'o_orca_track_1.mp3',
-'o_orca_track_2.mov' => 'o_orca_track_2.mp3',
-'o_orca_track_3.mov' => 'o_orca_track_3.mp3',
-'o_orca_track_4.mov' => 'o_orca_track_4.mp3',
-'o_orca_track_5.mov' => 'o_orca_track_5.mp3',
-'o_rosmarus_track_1.mov' => 'o_rosmarus_track_1.mp3',
-'p_crassidens_track_1.mov' => 'p_crassidens_track_1.mp3',
-'p_groenlandica_track_1.mov' => 'p_groenlandica_track_1.mp3',
-'p_macrocephalus_track_1.mov' => 'p_macrocephalus_track_1.mp3',
-'p_macrocephalus_track_2.mov' => 'p_macrocephalus_track_2.mp3',
-'p_macrocephalus_track_3.mov' => 'p_macrocephalus_track_3.mp3',
-'p_macrocephalus_track_4.mov' => 'p_macrocephalus_track_4.mp3',
-'p_macrocephalus_track_5.mov' => 'p_macrocephalus_track_5.mp3',
-'p_macrocephalus_track_6.mov' => 'p_macrocephalus_track_6.mp3',
-'p_macrocephalus_track_7.mov' => 'p_macrocephalus_track_7.mp3',
-'p_macrocephalus_track_8.mov' => 'p_macrocephalus_track_8.mp3',
-'p_phocoena_track_1.mov' => 'p_phocoena_track_1.mp3',
-'t_truncatus_track_1.mov' => 't_truncatus_track_1.mp3',
-
-'d_delphis_movie_1.mov' => 'd_delphis_movie_1.mp4',
-'d_delphis_movie_2.mov' => 'd_delphis_movie_2.mp4',
-'g_macrorhynchus_movie.mov' => 'g_macrorhynchus_movie.mp4',
-'s_attenuata_movie.mov' => 's_attenuata_movie.mp4',
-'s_bredanensis_movie.mov' => 's_bredanensis_movie.mp4',
-'s_coeruleoalba_movie.mov' => 's_coeruleoalba_movie.mp4',
-'t_truncatus_movie.mov' => 't_truncatus_movie.mp4',
-
+'bioluminescence_1_spiraling.mov' => 'bioluminescence_1_spiraling.mp4',
+'bioluminescence_2.mov' => 'bioluminescence_2.mp4',
+'bioluminescence_3_hovering.mov' => 'bioluminescence_3_hovering.mp4',
+'chromatophores.mov' => 'chromatophores.mp4',
+'dense_school_of_krill.mov' => 'dense_school_of_krill.mp4',
+'diver_with_krill_school.mov' => 'diver_with_krill_school.mp4',
+'epacifica_gills_eye__lappet.mov' => 'epacifica_gills_eye__lappet.mp4',
+'escape_response.mov' => 'escape_response.mp4',
+'eyestalk_rotation.mov' => 'eyestalk_rotation.mp4',
+'furcilia_1_dorsal_scan.mov' => 'furcilia_1_dorsal_scan.mp4',
+'hindgut_peristalsis.mov' => 'hindgut_peristalsis.mp4',
+'hovering__spiral_swimming.mov' => 'hovering__spiral_swimming.mp4',
+'internal_spermatophore.mov' => 'internal_spermatophore.mp4',
+'krill_feeding_on_ice_algae.mov' => 'krill_feeding_on_ice_algae.mp4',
+'krill_schools.mov' => 'krill_schools.mp4',
+'ontogeny_of_swimming.mov' => 'ontogeny_of_swimming.mp4',
+'parasitic_dajid_isopod.mov' => 'parasitic_dajid_isopod.mp4',
+'photophore_rotation.mov' => 'photophore_rotation.mp4',
+'pleopod__thoracic_limb_moti.mov' => 'pleopod__thoracic_limb_moti.mp4',
+'pleopod_motion_epacifica.mov' => 'pleopod_motion_epacifica.mp4',
+'pleopod_motion_n_difficilis.mov' => 'pleopod_motion_n_difficilis.mp4',
+'pleopod_motion_n_simplex.mov' => 'pleopod_motion_n_simplex.mp4',
+'small_school_of_krill.mov' => 'small_school_of_krill.mp4',
+'stomach__hindgut_contractio.mov' => 'stomach__hindgut_contractio.mp4',
+'thoracic_leg_2__egg_mass.mov' => 'thoracic_leg_2__egg_mass.mp4',
+'thoracic_leg_2_n_difficilis.mov' => 'thoracic_leg_2_n_difficilis.mp4',
 
 
 
