7ec5ed4e02a0301f27a8a1654225a33d90ad5ed3
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2016-06-17 15:27:13 +0200

    butterflies mov

--- tools/media/mov_replace.php
+++ tools/media/mov_replace.php
@@ -1,36 +1,30 @@
 <?php
 
-    $projectId = 8;
+    $projectId = 162;
 
 
 $replacement = array(
 
-'bioluminescence_1_spiraling.mov' => 'bioluminescence_1_spiraling.mp4',
-'bioluminescence_2.mov' => 'bioluminescence_2.mp4',
-'bioluminescence_3_hovering.mov' => 'bioluminescence_3_hovering.mp4',
-'chromatophores.mov' => 'chromatophores.mp4',
-'dense_school_of_krill.mov' => 'dense_school_of_krill.mp4',
-'diver_with_krill_school.mov' => 'diver_with_krill_school.mp4',
-'epacifica_gills_eye__lappet.mov' => 'epacifica_gills_eye__lappet.mp4',
-'escape_response.mov' => 'escape_response.mp4',
-'eyestalk_rotation.mov' => 'eyestalk_rotation.mp4',
-'furcilia_1_dorsal_scan.mov' => 'furcilia_1_dorsal_scan.mp4',
-'hindgut_peristalsis.mov' => 'hindgut_peristalsis.mp4',
-'hovering__spiral_swimming.mov' => 'hovering__spiral_swimming.mp4',
-'internal_spermatophore.mov' => 'internal_spermatophore.mp4',
-'krill_feeding_on_ice_algae.mov' => 'krill_feeding_on_ice_algae.mp4',
-'krill_schools.mov' => 'krill_schools.mp4',
-'ontogeny_of_swimming.mov' => 'ontogeny_of_swimming.mp4',
-'parasitic_dajid_isopod.mov' => 'parasitic_dajid_isopod.mp4',
-'photophore_rotation.mov' => 'photophore_rotation.mp4',
-'pleopod__thoracic_limb_moti.mov' => 'pleopod__thoracic_limb_moti.mp4',
-'pleopod_motion_epacifica.mov' => 'pleopod_motion_epacifica.mp4',
-'pleopod_motion_n_difficilis.mov' => 'pleopod_motion_n_difficilis.mp4',
-'pleopod_motion_n_simplex.mov' => 'pleopod_motion_n_simplex.mp4',
-'small_school_of_krill.mov' => 'small_school_of_krill.mp4',
-'stomach__hindgut_contractio.mov' => 'stomach__hindgut_contractio.mp4',
-'thoracic_leg_2__egg_mass.mov' => 'thoracic_leg_2__egg_mass.mp4',
-'thoracic_leg_2_n_difficilis.mov' => 'thoracic_leg_2_n_difficilis.mp4',
+'001.mov' => '001.mp4',
+'002.mov' => '002.mp4',
+'003.mov' => '003.mp4',
+'004.mov' => '004.mp4',
+'005.mov' => '005.mp4',
+'006.mov' => '006.mp4',
+'007.mov' => '007.mp4',
+'008.mov' => '008.mp4',
+'009.mov' => '009.mp4',
+'010.mov' => '010.mp4',
+'011.mov' => '011.mp4',
+'012.mov' => '012.mp4',
+'013.mov' => '013.mp4',
+'014.mov' => '014.mp4',
+'015.mov' => '015.mp4',
+'016.mov' => '016.mp4',
+'017.mov' => '017.mp4',
+'018.mov' => '018.mp4',
+'019.mov' => '019.mp4',
+'020.mov' => '020.mp4',
 
 
 
