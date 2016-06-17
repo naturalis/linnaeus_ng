24ff9d13ab9c6ca8c62892416a8a02ad972a24ed
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2016-06-17 17:04:50 +0200

    rept uk mov

--- tools/media/mov_replace.php
+++ tools/media/mov_replace.php
@@ -1,23 +1,21 @@
 <?php
 
-    $projectId = 67;
+    $projectId = 135;
 
 
 $replacement = array(
 
-'alarmroep juvenielen.mov' => 'alarmroep juvenielen.mp3',
-'paarroep bastaardkikker.mov' => 'paarroep bastaardkikker.mp3',
-'paarroep boomkikker.mov' => 'paarroep boomkikker.mp3',
-'paarroep bruine kikker.mov' => 'paarroep bruine kikker.mp3',
-'paarroep geelbuikvuurpad.mov' => 'paarroep geelbuikvuurpad.mp3',
-'paarroep gewone pad.mov' => 'paarroep gewone pad.mp3',
-'paarroep heikikker.mov' => 'paarroep heikikker.mp3',
-'paarroep meerkikker.mov' => 'paarroep meerkikker.mp3',
-'paarroep poelkikker.mov' => 'paarroep poelkikker.mp3',
-'paarroep rugstreeppad.mov' => 'paarroep rugstreeppad.mp3',
-'paarroep stierkikker.mov' => 'paarroep stierkikker.mp3',
-'paarroep vroedmeesterpad.mov' => 'paarroep vroedmeesterpad.mp3',
-'paarroep_boomkikker.mov' => 'paarroep_boomkikker.mp3',
+'alarm call juveniles.mov' => 'alarm call juveniles.mp3',
+'call american bullfrog.mov' => 'call american bullfrog.mp3',
+'call common frog.mov' => 'call common frog.mp3',
+'call common toad.mov' => 'call common toad.mp3',
+'call edible frog.mov' => 'call edible frog.mp3',
+'call european tree frog.mov' => 'call european tree frog.mp3',
+'call marsh frog.mov' => 'call marsh frog.mp3',
+'call midwife toad.mov' => 'call midwife toad.mp3',
+'call natterjack toad.mov' => 'call natterjack toad.mp3',
+'call pool frog.mov' => 'call pool frog.mp3',
+'call yellow-bellied toad.mov' => 'call yellow-bellied toad.mp3',
 
 );
 
