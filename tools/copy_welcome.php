64385b31c8dbc139e8465a949e7da963b2a2e2a9
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2016-06-22 14:20:17 +0200

    ja lekker

--- tools/copy_welcome.php
+++ tools/copy_welcome.php
@@ -17,25 +17,28 @@
     foreach ($projects as $projectId) {
     	$q = 'select * from content where project_id = ' . $projectId;
         $r = mysqli_query($my, $q);
-        $d = mysqli_fetch_all($r, MYSQLI_ASSOC);
 
         mysqli_query($my, 'update introduction_pages set show_order = show_order + 2
             where project_id = '. $projectId);
 
-        foreach ($d as $i => $row) {
+        $i = 0;
+
+        while ($row = mysqli_fetch_assoc($r)) {
+
             $q = 'insert into introduction_pages
                   (project_id, got_content, show_order, hide_from_index, created, last_change)
                   values
                   (' . $projectId . ',1,' . $i . ',0,now(),now())';
-            $r = mysqli_query($my, $q) or die($q);
+            mysqli_query($my, $q) or die($q);
             $pageId = mysqli_insert_id($my);
 
             $q = 'insert into content_introduction values
                   (null,' . $projectId . ',' . $pageId . ', ' . $row['language_id'] . ', "' .
                   mysqli_real_escape_string($my, $row['subject']) . '","' .
                   mysqli_real_escape_string($my, $row['content']) . '",now(),now())';
-            $r = mysqli_query($my, $q) or die($q);
+            mysqli_query($my, $q) or die($q);
 
+            $i++;
         }
     }
 ?>
