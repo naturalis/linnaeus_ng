2b387d9d01f87b1d499f2b7185b2f7c42cddcf91
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2016-06-30 13:23:22 +0200

    update webservice

--- configuration/admin/default-configuration.php
+++ configuration/admin/default-configuration.php
@@ -4,7 +4,7 @@ ini_set('memory_limit', '512M');
 if(file_exists( __DIR__ . '/custom-configuration.php')) include_once( __DIR__ . '/custom-configuration.php' );
 class configuration
 {
-    
+
     private $_appFileRoot;
 
     public function __construct ()
@@ -19,11 +19,11 @@ class configuration
     public function getDatabaseSettings ()
     {
         return array(
-            'host' => '@DB.HOST@', 
-            'user' => 'linnaeus_user', 
-            'password' => 'car0lu5', 
-            'database' => 'linnaeus_ng', 
-            'tablePrefix' => 'dev_', 
+            'host' => '@DB.HOST@',
+            'user' => 'linnaeus_user',
+            'password' => 'car0lu5',
+            'database' => 'linnaeus_ng',
+            'tablePrefix' => 'dev_',
             'characterSet' => 'utf8'
         );
     }
@@ -31,10 +31,10 @@ class configuration
     public function getSmartySettings ()
     {
         return array(
-            'dir_template' => $this->_appFileRoot . 'www/admin/templates/templates', 
-            'dir_compile' => $this->_appFileRoot . 'www/admin/templates/templates_c', 
-            'dir_cache' => $this->_appFileRoot . 'www/admin/templates/cache', 
-            'dir_config' => $this->_appFileRoot . 'www/admin/templates/configs', 
+            'dir_template' => $this->_appFileRoot . 'www/admin/templates/templates',
+            'dir_compile' => $this->_appFileRoot . 'www/admin/templates/templates_c',
+            'dir_cache' => $this->_appFileRoot . 'www/admin/templates/cache',
+            'dir_config' => $this->_appFileRoot . 'www/admin/templates/configs',
             'caching' => 1,  // 1,
             'compile_check' => true
         );
@@ -45,26 +45,26 @@ class configuration
         return array(
             'app' => array(
 				'applicationFileRoot' => $this->_appFileRoot,
-                'name' => 'Linnaeus NG Administration', 
-                'version' => '@APP.VERSION@', 
-                'versionTimestamp' => '@TIMESTAMP@', 
+                'name' => 'Linnaeus NG Administration',
+                'version' => '@APP.VERSION@',
+                'versionTimestamp' => '@TIMESTAMP@',
                 'pathName' => 'admin',
             ),
 			'serverTimeZone' => 'Europe/Amsterdam',
             'paths' => array(
-                'login' => '/views/users/login.php', 
-                'logout' => '/views/users/logout.php', 
-                'chooseProject' => '/views/users/choose_project.php', 
-                'projectIndex' => '/views/utilities/admin_index.php', 
-                'notAuthorized' => '/views/utilities/not_authorized.php', 
+                'login' => '/views/users/login.php',
+                'logout' => '/views/users/logout.php',
+                'chooseProject' => '/views/users/choose_project.php',
+                'projectIndex' => '/views/utilities/admin_index.php',
+                'notAuthorized' => '/views/utilities/not_authorized.php',
                 'moduleNotPresent' => '/views/utilities/module_not_present.php',
-                'mediaBasePath' => '../../../shared/media/project', 
-            ), 
+                'mediaBasePath' => '../../../shared/media/project',
+            ),
             'directories' => array(
-                'mediaDirProject' => $this->_appFileRoot . 'www/shared/media/project', 
+                'mediaDirProject' => $this->_appFileRoot . 'www/shared/media/project',
 				'log' => $this->_appFileRoot . 'log',
                 'runtimeStyleRoot' => $this->_appFileRoot . 'www/app/style',
-            ), 
+            ),
             'login-cookie' => array(
                 'name' => 'linnaeus-login',
                 'lifetime' => 30, // days
@@ -75,7 +75,8 @@ class configuration
 			'soundPlayerName' => 'player_mp3.swf',
 			'useJavascriptLinks' => false,
 			'projectCssTemplateFile' => 'project-template.css',
-        	'appNameFrontEnd' => 'app'
+        	'appNameFrontEnd' => 'app',
+            'pushUrl' => 'http://linnaeus.naturalis.nl/admin/server_csv.php'
         );
     }
 
