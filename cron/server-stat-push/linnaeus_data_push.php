b801db1974c36d81d0ae045f60e07de0ff2920ec
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2016-07-04 16:57:08 +0200

    push cron

--- cron/server-stat-push/linnaeus_data_push.php
+++ cron/server-stat-push/linnaeus_data_push.php
@@ -2,17 +2,22 @@
 
     class LinnaeusDataPush
     {
-
-	    private $config;
+		private $config;
 	    private $mysqli;
+	    private $tablePrefix;
 	    private $data = array();
 	    private $pushUrl;
         private $timeout;
         private $pushResult;
+        private $gitRepo;
+        private $gitBranch;
+        private $gitHash;
+        private $gitLatestHash;
 
         public function __construct () {
             $this->getConfig();
             $this->connectDb();
+            $this->setGit();
         }
 
 		public function run () {
@@ -36,8 +41,32 @@
 		private function bootstrap ()
 		{
             if (empty($this->pushUrl)) {
-                throw new Exception("Push url not set\n");
+                die("Push url not set");
             }
+		    if (empty($this->gitBranch)) {
+                die("Git branch not set");
+            }
+			if (empty($this->gitHash)) {
+                die("Git hash not set");
+            }
+			if (empty($this->gitLatestHash)) {
+                die("Git latest hash not set");
+            }
+		}
+
+		private function setGit ()
+		{
+            $c = $this->config->getGeneralSettings();
+            exec('cd ' . str_replace(" ", "\\ ", $c['applicationFileRoot']));
+
+            exec('git rev-parse --abbrev-ref HEAD', $branch) or die("Git branch not set\n");
+		    $this->gitBranch = $branch[0];
+
+            exec('git rev-parse HEAD', $hash) or die("Git hash not set\n");
+		    $this->gitHash = $hash[0];
+
+		    exec('git rev-parse origin/' . $this->gitBranch, $latestHash);
+		    $this->gitLatestHash = $latestHash[0];
 		}
 
 		private function pushData ()
@@ -65,22 +94,20 @@
 
 		private function setData ()
 		{
-		    exec('git rev-parse --abbrev-ref HEAD', $branch);
-            exec('git rev-parse HEAD', $hash);
-            exec('git rev-parse origin/' . $branch[0], $latestHash);
-
         	foreach ($this->data as $i => $row) {
-                $this->data[$i]['git_branch'] = $branch[0];
-                $this->data[$i]['git_hash'] = $hash[0];
-                $this->data[$i]['git_latest_hash'] = $latestHash[0];
+                $this->data[$i]['git_branch'] = $this->gitBranch;
+                $this->data[$i]['git_hash'] = $this->gitHash;
+                $this->data[$i]['git_latest_hash'] = $this->gitLatestHash;
     	        $this->data[$i]['project_is_published'] =
                     ($this->data[$i]['project_is_published'] == 1) ? 'yes' : 'no';
                 $this->data[$i]['user_is_active'] =
                     ($this->data[$i]['user_is_active'] == 1) ? 'yes' : 'no';
                 $this->data[$i]['code_up_to_date'] =
                     ($this->data[$i]['git_hash'] == $this->data[$i]['git_latest_hash']) ? 'yes' : 'no';
-                $this->data[$i]['server_ip'] = $_SERVER['SERVER_ADDR'];
-                $this->data[$i]['server_name'] = gethostbyaddr($this->data[$i]['server_ip']);
+                $this->data[$i]['server_ip'] = isset($_SERVER['SERVER_ADDR']) ?
+                    $_SERVER['SERVER_ADDR'] : null;
+                $this->data[$i]['server_name'] = !is_null($this->data[$i]['server_ip']) ?
+                    gethostbyaddr($this->data[$i]['server_ip']) : null;
                 $this->data[$i]['check_date'] = date("Y-m-d H:m:s");
         	}
 		}
@@ -89,6 +116,11 @@
 		{
             require_once dirname(__FILE__) . '/../../configuration/admin/configuration.php';
             $this->config = new configuration();
+		}
+
+		private function printResult ()
+		{
+		    die($this->pushResult->result . "\n");
 		}
 
         private function getProjectsWithUsers ()
@@ -107,13 +139,13 @@
                     t2.last_project_select as project_last_selected,
                     t3.last_password_change as password_last_changed
                 from
-                    ' . $this->mysqli->prefix . 'projects as t1
+                    ' . $this->tablePrefix . 'projects as t1
                 left join
-                    ' . $this->mysqli->prefix . 'projects_roles_users as t2 on t1.id = t2.project_id
+                    ' . $this->tablePrefix . 'projects_roles_users as t2 on t1.id = t2.project_id
                 left join
-                    ' . $this->mysqli->prefix . 'users as t3 on t2.user_id = t3.id
+                    ' . $this->tablePrefix . 'users as t3 on t2.user_id = t3.id
                 left join
-                    ' . $this->mysqli->prefix . 'roles as t4 on t2.role_id = t4.id
+                    ' . $this->tablePrefix . 'roles as t4 on t2.role_id = t4.id
                 order by
                     t1.sys_name, t3.username';
 
@@ -126,33 +158,17 @@
 
     	private function connectDb()
 		{
-			$data = $this->config->getDatabaseSettings();
+			$c = $this->config->getDatabaseSettings();
+			$this->tablePrefix = $c['tablePrefix'];
 
-		    try {
-				$this->mysqli = new stdClass();
-				$this->mysqli->user = $data['user'];
-				$this->mysqli->password = $data['password'];
-				$this->mysqli->host = $data['host'];
-				$this->mysqli->database = $data['database'];
-				$this->mysqli->prefix = $data['tablePrefix'];
-				$this->mysqli->character_set = $data['characterSet'];
-			} catch (Exception $e) {
-			    echo $e->getMessage($e) . "\n";
-			}
-
-		    $this->mysqli = new mysqli(
-				$this->mysqli->host,
-				$this->mysqli->user,
-				$this->mysqli->password,
-				$this->mysqli->database
-			);
+		    $this->mysqli = new mysqli($c['host'], $c['user'], $c['password'], $c['database']);
 
 			if ($this->mysqli->connect_error) {
 				throw new Exception($this->mysqli->connect_error . "\n");
 			}
 
-			$this->mysqli->query('SET NAMES ' . $this->mysqli->character_set);
-			$this->mysqli->query('SET CHARACTER SET ' . $this->mysqli->character_set);
+			$this->mysqli->query('SET NAMES ' . $c['characterSet']);
+			$this->mysqli->query('SET CHARACTER SET ' . $c['characterSet']);
 		}
     }
 
