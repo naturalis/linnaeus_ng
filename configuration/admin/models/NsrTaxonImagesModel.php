99da16342b20d2d41e09464ea98ab5de72509f3e
maarten schermer <maarten.schermer@naturalis.nl>
2016-08-10 11:44:43 +0200

    better admin:img:mmeta_batch NSR ID resolvement

--- configuration/admin/models/NsrTaxonImagesModel.php
+++ configuration/admin/models/NsrTaxonImagesModel.php
@@ -250,12 +250,15 @@ final class NsrTaxonImagesModel extends AbstractModel
 			where
 				_a.project_id =".$project_id."
 				and ifnull(_trash.is_deleted,0)=0
-				and (
-					nsr_id = '".$nsr_id."' or
-					nsr_id = 'concept/".$nsr_id."' or
-					nsr_id = 'tn.nlsr.concept/".$nsr_id."'
-				)
+				and substr(nsr_id,-1 * length('" .$nsr_id ."'))='" . $nsr_id ."'
 		";
+/*
+	and (
+		nsr_id = '".$nsr_id."' or
+		nsr_id = 'concept/".$nsr_id."' or
+		nsr_id = 'tn.nlsr.concept/".$nsr_id."'
+	)
+*/
 			
 		return $this->freeQuery( $query );
 	
