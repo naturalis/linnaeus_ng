4b32dc519d16051c7072383e945a2a8abea15c9e
maarten schermer <maarten.schermer@naturalis.nl>
2016-08-16 14:12:00 +0200

    webservice imagesAction + suppress parent checks

--- configuration/admin/controllers/NsrTaxonController.php
+++ configuration/admin/controllers/NsrTaxonController.php
@@ -78,6 +78,8 @@ class NsrTaxonController extends NsrController
 		$this->moduleSettings=new ModuleSettingsReaderController;
 		$this->_taxon_main_image_base_url=$this->moduleSettings->getGeneralSetting( 'taxon_main_image_base_url' );
 		$this->smarty->assign( 'taxon_main_image_base_url',$this->_taxon_main_image_base_url );
+
+		$this->_suppress_parent_child_relation_checks=$this->moduleSettings->getModuleSetting( [ 'module'=>'species','setting'=>'suppress_parent_child_relation_checks', 'subst'=>0 ] )==1;
 	}
 
     public function taxonNewAction()
@@ -991,8 +993,25 @@ class NsrTaxonController extends NsrController
 
 	private function checkParentChildRelationship($child_base_rank,$parent_id)
 	{
+		
 		$d=$this->getTaxonById($parent_id);
+
 		$parent_base_rank=$d['base_rank'];
+
+		if ( $this->_suppress_parent_child_relation_checks )
+		{
+			if ( $parent_base_rank < $child_base_rank )
+			{
+				$this->setMessage( $this->translate("Concept saved with irregular parent (allowed through the 'suppress_parent_child_relation_checks' setting).") );
+				return true;
+			}
+			else
+			{
+				$this->addError( $this->translate("Parent rank cannot be same as, or below concept rank (even with 'suppress_parent_child_relation_checks' setting in effect).") );
+				$this->addError( $this->translate("Concept not saved.") );
+				return false;			
+			}
+		}
 
 		$ranks=$this->models->Ranks->_get(array(
 			'id'=>'*',
@@ -1123,6 +1142,11 @@ class NsrTaxonController extends NsrController
 		{
 			// subrijk moet onder rijk
 			$error=array('regnum');
+		}
+		else
+		if ( $parent_base_rank >= $child_base_rank )
+		{
+			$error=array('a higher rank');
 		}
 
 		if ($error)
