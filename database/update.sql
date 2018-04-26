55cc8f6442d3ed35901a21802c62db8636faf043
ruud-altenburg <ruud.altenburg@naturalis.nl>
2018-04-26 12:14:19 +0200

    LINNA-1188: traits tabellen in update script

--- database/update.sql
+++ database/update.sql
@@ -1,3 +1,25 @@
+INSERT IGNORE INTO `traits_date_formats` VALUES 
+(1,'year','Y','JJJJ','%Y',0,now(),now()),
+(2,'fulldate','d-m-Y','DD-MM-JJJJ','%Y-%m-%d',1,now(),now());
+
+INSERT IGNORE INTO `traits_types` VALUES 
+(1,'boolean',1,2,'check_boolean',0,0,0,0,0,0,0,now(),now()),
+(2,'stringlist',3,4,'check_stringlist',1,1,1,0,1,0,0,now(),now()),
+(3,'stringlistfree',5,6,'check_stringlistfree',1,1,1,0,1,0,0,now(),now()),
+(4,'stringfree',7,8,'check_stringfree',0,1,1,0,0,0,0,now(),now()),
+(5,'intlist',9,NULL,'check_intlist',1,1,1,0,1,1,1,now(),now()),
+(6,'intlistfree',10,11,'check_intlistfree',1,1,1,0,1,1,1,now(),now()),
+(7,'intfree',12,13,'check_intfree',0,1,1,0,0,1,1,now(),now()),
+(8,'intfreelimit',14,15,'check_intfreelimit',1,1,1,0,0,1,1,now(),now()),
+(9,'floatlist',16,17,'check_floatlist',1,1,0,1,1,1,1,now(),now()),
+(10,'floatlistfree',18,19,'check_floatlistfree',1,1,0,1,1,1,1,now(),now()),
+(11,'floatfree',20,21,'check_floatfree',0,1,0,1,0,1,1,now(),now()),
+(12,'floatfreelimit',22,23,'check_floatfreelimit',1,1,0,1,0,1,1,now(),now()),
+(13,'datelist',24,NULL,'check_datelist',1,0,0,0,1,1,1,now(),now()),
+(14,'datelistfree',25,26,'check_datelistfree',1,0,0,0,1,1,1,now(),now()),
+(15,'datefree',27,NULL,'check_datefree',0,0,0,0,0,1,1,now(),now()),
+(16,'datefreelimit',28,29,'check_datefreelimit',1,0,0,0,0,1,1,now(),now());
+
 
 select (@id := id), (@project_id := project_id) from literature2_publication_types where sys_label = 'Artikel';
 insert ignore into literature2_publication_types_labels values (null, @project_id, @id, 26, 'Article', now(), now());
