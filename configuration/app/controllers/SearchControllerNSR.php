0612535385ea1132cb5865506bfff67831bb899b
ruud-altenburg <ruud.altenburg@naturalis.nl>
2018-06-07 13:44:07 +0200

    LINNA-1069

--- configuration/app/controllers/SearchControllerNSR.php
+++ configuration/app/controllers/SearchControllerNSR.php
@@ -193,12 +193,12 @@ class SearchControllerNSR extends SearchController
     public function searchExtendedAction()
     {
 		$search=$this->rGetAll();
-
+		$this->smarty->assign('url_taxon_detail',"http://". $this->httpHost.'/linnaeus_ng/'.$this->getAppname().'/views/species/taxon.php?id=');
+		
 		if ($this->rHasVal('action','export')) {
 			$search['limit']=1000;
 			$template='export_search_extended';
 			$this->smarty->assign('csvExportSettings',$this->csvExportSettings);
-			$this->smarty->assign('url_taxon_detail',"http://". $this->httpHost.'/nsr/concept/');
 
 			$this->downloadHeaders(
 				array(
@@ -210,7 +210,6 @@ class SearchControllerNSR extends SearchController
 			$this->smarty->assign('search',$search);
 			$this->smarty->assign('querystring',$this->reconstructQueryString(array('search'=>$search,'ignore'=>array('page'))));
 			$this->smarty->assign('presence_statuses',$this->getPresenceStatuses());
-			$this->smarty->assign('url_taxon_detail',"http://". $this->httpHost.'/linnaeus_ng/'.$this->getAppname().'/views/species/taxon.php?id=');
 			$template=null;
 		}
 
@@ -504,8 +503,8 @@ class SearchControllerNSR extends SearchController
 			$data[$key]['taxon']=$this->addHybridMarkerAndInfixes( [ 'name'=>$val['taxon'],'base_rank_id'=>$val['base_rank_id'],'taxon_id'=>$val['taxon_id'],'parent_id'=>$val['parent_id'] ] );
 			$data[$key]['taxon_download']=html_entity_decode(strip_tags($data[$key]['taxon']));
 			$data[$key]['overview_image']=$this->getTaxonOverviewImage($val['taxon_id']);
-			if ( $this->_show_all_preferred_names_in_results )
-			{
+			
+			if ( $this->_show_all_preferred_names_in_results ) {
 				$data[$key]['common_names']=
 					$this->models->Names->_get( [ 'id' => [
 						'project_id'=>$this->getCurrentProjectId(),
