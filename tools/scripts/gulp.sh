7be47661dbd0868e68b8ea0deab651fbad9abea7
Joep Vermaat <joep.vermaat@naturalis.nl>
2019-10-23 14:02:47 +0200

    Downgrade to node 10

--- tools/scripts/gulp.sh
+++ tools/scripts/gulp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /var/www/html/
 /usr/bin/npm install -g n                                                                                       
-/usr/bin/n stable   
+/usr/bin/n 10.16.3
 /usr/bin/npm install --global yarn
 /usr/bin/npm install --global gulp
 /usr/local/bin/yarn
