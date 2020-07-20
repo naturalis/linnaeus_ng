dc8d6347bde8779a14b525e9e24bebd2c27c4b97
Joep Vermaat <joep.vermaat@naturalis.nl>
2020-07-20 17:13:37 +0200

    Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var concat = require('gulp-concat');
 var terser = require('gulp-terser');
-var gutil = require('gulp-util');
+var log = require('fancy-log');
 var modernizr = require('gulp-modernizr');
 
 gulp.task('modernizr', function() {
@@ -12,13 +12,13 @@ gulp.task('modernizr', function() {
     .pipe(modernizr())
     .pipe(gulp.dest('node_modules/modernizr/'))
 });
-gulp.task('admin-bundle', ['modernizr'], function () {
+gulp.task('admin-bundle', gulp.series('modernizr'), function () {
     // bundeling the essential admin javascript
     return browserify('./gulp/admin.js').bundle()
         .pipe(source('bundle.js'))
         .pipe(buffer())
         .pipe(terser())
-        .on('error', gutil.log)
+        .on('error', log)
         .pipe(gulp.dest('./www/admin/vendor/'));
 });
 // @todo: maybe do this some other time
@@ -37,7 +37,7 @@ gulp.task('admin-copy', function () {
     ], { base: 'node_modules/jQuery/dist' })
         .pipe(gulp.dest('./www/admin/vendor/jQuery'));
     */
-    gulp.src([
+    return gulp.src([
         'node_modules/dropzone/dist/**/*',
         'node_modules/prettyPhoto/**/*',
         'node_modules/ckeditor/**/*',
@@ -46,21 +46,17 @@ gulp.task('admin-copy', function () {
         'node_modules/nestedSortable/jquery.mjs.nestedSortable.js'
     ], { base: 'node_modules' })
         .pipe(gulp.dest('./www/admin/vendor'));
-    gulp.src([
-        'node_modules/\@bower_components/Javascript InfoVis Toolkit/Jit/*.js'
-    ],{ base: 'node_modules/\@bower_components/Javascript InfoVis Toolkit'})
-        .pipe(gulp.dest('./www/admin/vendor'));
 });
-gulp.task('app-bundle', ['modernizr'], function () {
+gulp.task('app-bundle', gulp.series('modernizr'), function () {
     // bundling essential app javascript
     return browserify('./gulp/app.js').bundle()
         .pipe(source('bundle.js'))
         .pipe(buffer())
         .pipe(terser())
-        .on('error', gutil.log)
+        .on('error', log)
         .pipe(gulp.dest('./www/app/vendor/'));
 });
-gulp.task('app-copy', function () {
+gulp.task('app-copy', function (done) {
     // copying essential app javascript plugins
     /*
     gulp.src([
@@ -76,15 +72,15 @@ gulp.task('app-copy', function () {
     ], { base: 'node_modules' })
         .pipe(gulp.dest('./www/app/vendor'));
     gulp.src([
-        'node_modules/\@bower_components/fancybox/dist/**/*'
-    ], { base: 'node_modules/\@bower_components/fancybox/dist/' })
+        'node_modules/\@fancyapps/fancybox/dist/**/*'
+    ], { base: 'node_modules/\@fancyapps/fancybox/dist/' })
         .pipe(gulp.dest('./www/app/vendor/fancybox'));
+    done();
 });
 gulp.task('default',
-    [
+    gulp.series(
         'admin-bundle',
         'admin-copy',
         'app-bundle',
-        'app-copy'
-    ]
+        'app-copy')
 );
